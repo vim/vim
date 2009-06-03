535c54008561ffea126c2c083d56312307692a0e
Bram Moolenaar <Bram@vim.org>
2009-06-03 13:05:07 +0000

    updated for version 7.2-194

--- src/msvc2008.bat
+++ src/msvc2008.bat
@@ -1,5 +1,7 @@
+@echo off
 rem To be used on MS-Windows for Visual C++ 2008 Express Edition
 rem   aka Microsoft Visual Studio 9.0.
 rem See INSTALLpc.txt for information.
+@echo on
 
 call "%VS90COMNTOOLS%%vsvars32.bat"
