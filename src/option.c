801f8b865c7e093ce2a2f96221bfbfd4779da187
Bram Moolenaar <Bram@vim.org>
2009-07-29 13:42:05 +0000

    updated for version 7.2-242

--- src/option.c
+++ src/option.c
@@ -7194,6 +7194,14 @@ set_bool_option(opt_idx, varp, value, opt_flags)
 	compatible_set();
     }
 
+    /* 'list', 'number' */
+    else if ((int *)varp == &curwin->w_p_list
+	  || (int *)varp == &curwin->w_p_nu)
+    {
+	if (curwin->w_curswant != MAXCOL)
+	    curwin->w_set_curswant = TRUE;
+    }
+
     else if ((int *)varp == &curbuf->b_p_ro)
     {
 	/* when 'readonly' is reset globally, also reset readonlymode */
@@ -7645,6 +7653,14 @@ set_bool_option(opt_idx, varp, value, opt_flags)
 	    curbuf->b_p_imsearch = B_IMODE_USE_INSERT;
 # endif
 	}
+	if (curwin->w_curswant != MAXCOL)
+	    curwin->w_set_curswant = TRUE;
+    }
+
+    else if ((int *)varp == &p_arshape)
+    {
+	if (curwin->w_curswant != MAXCOL)
+	    curwin->w_set_curswant = TRUE;
     }
 #endif
 
@@ -7655,8 +7671,7 @@ set_bool_option(opt_idx, varp, value, opt_flags)
     options[opt_idx].flags |= P_WAS_SET;
 
     comp_col();			    /* in case 'ruler' or 'showcmd' changed */
-    if (curwin->w_curswant != MAXCOL)
-	curwin->w_set_curswant = TRUE;  /* in case 'list' changed */
+
     check_redraw(options[opt_idx].flags);
 
     return NULL;
