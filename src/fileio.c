8cac9fd020f2c4b70856939b098a00546f2009ce
Bram Moolenaar <Bram@vim.org>
2010-03-02 12:48:05 +0100

    updated for version 7.2.379 Problem:    'eventignore' is set to an invalid value inside ":doau". (Antony             Scriven) Solution:   Don't include the leading comma when the option was empty.

--- src/fileio.c
+++ src/fileio.c
@@ -7925,7 +7925,10 @@ au_event_disable(what)
 	new_ei = vim_strnsave(p_ei, (int)(STRLEN(p_ei) + STRLEN(what)));
 	if (new_ei != NULL)
 	{
-	    STRCAT(new_ei, what);
+	    if (*what == ',' && *p_ei == NUL)
+		STRCPY(new_ei, what + 1);
+	    else
+		STRCAT(new_ei, what);
 	    set_string_option_direct((char_u *)"ei", -1, new_ei,
 							  OPT_FREE, SID_NONE);
 	    vim_free(new_ei);
