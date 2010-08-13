580061a821eb2e08439233cbeabada67c480493b
Bram Moolenaar <Bram@vim.org>
2010-08-13 13:57:13 +0200

    Fix #ifdef for GDK_SUPER_MASK.

--- src/gui_gtk_x11.c
+++ src/gui_gtk_x11.c
@@ -889,7 +889,7 @@ modifiers_gdk2vim(guint state)
 	modifiers |= MOD_MASK_CTRL;
     if (state & GDK_MOD1_MASK)
 	modifiers |= MOD_MASK_ALT;
-#ifdef GDK_SUPER_MASK
+#if GTK_CHECK_VERSION(2,10,0)
     if (state & GDK_SUPER_MASK)
 	modifiers |= MOD_MASK_META;
 #endif
