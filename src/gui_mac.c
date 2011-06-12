d8644bd64645f2ba64aeac44d22126d2fd63afc3
Bram Moolenaar <Bram@vim.org>
2011-06-12 20:33:38 +0200

    updated for version 7.3.208 Problem:    Early terminated if statement. Solution:   Remove the semicolon. (Lech Lorens)

--- src/gui_mac.c
+++ src/gui_mac.c
@@ -1840,7 +1840,7 @@ gui_mac_doInZoomClick(EventRecord *theEvent, WindowPtr whichWindow)
 	p.v -= gui.scrollbar_height;
     p.v -= p.v % gui.char_height;
     p.v += 2 * gui.border_width;
-    if (gui.which_scrollbars[SBAR_BOTTOM]);
+    if (gui.which_scrollbars[SBAR_BOTTOM])
 	p.v += gui.scrollbar_height;
 
     ZoomWindowIdeal(whichWindow, thePart, &p);
