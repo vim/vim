50f834dfb2a06e7e5ecf937bc32d6d711e5883a5
Bram Moolenaar <Bram@vim.org>
2011-09-21 13:40:17 +0200

    updated for version 7.3.316 Problem:    Crash when 'colorcolumn' is set and closing buffer. Solution:   Check for w_buffer to be NULL. (Yasuhiro Matsumoto)

--- src/option.c
+++ src/option.c
@@ -7036,6 +7036,9 @@ check_colorcolumn(wp)
     int		i;
     int		j = 0;
 
+    if (wp->w_buffer == NULL)
+	return NULL;  /* buffer was closed */
+
     for (s = wp->w_p_cc; *s != NUL && count < 255;)
     {
 	if (*s == '-' || *s == '+')
