530e7dfafd7e951ac36e39c87bf72ebdb4caa428
Bram Moolenaar <Bram@vim.org>
2013-02-06 13:38:02 +0100

    updated for version 7.3.801 Problem:    ":window set nu?" displays the cursor line. (Nazri Ramliy) Solution:   Do not update the cursor line when conceallevel is zero or the             screen has scrolled. (partly by Christian Brabandt)

--- src/window.c
+++ src/window.c
@@ -3991,9 +3991,10 @@ win_goto(wp)
 
 #ifdef FEAT_CONCEAL
     /* Conceal cursor line in previous window, unconceal in current window. */
-    if (win_valid(owp))
+    if (win_valid(owp) && owp->w_p_cole > 0 && !msg_scrolled)
 	update_single_line(owp, owp->w_cursor.lnum);
-    update_single_line(curwin, curwin->w_cursor.lnum);
+    if (curwin->w_p_cole > 0 && !msg_scrolled)
+	need_cursor_line_redraw = TRUE;
 #endif
 }
 
