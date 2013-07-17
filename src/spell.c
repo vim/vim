706d2de9c1e0567a9865c6c535fb7feebf475fd8
Bram Moolenaar <Bram@vim.org>
2013-07-17 17:35:13 +0200

    updated for version 7.4a.028 Problem:    Crash when spell checking in new buffer. Solution:   Set the b_p_key field.  (Mike Williams)

--- src/spell.c
+++ src/spell.c
@@ -9054,6 +9054,9 @@ open_spellbuf()
     {
 	buf->b_spell = TRUE;
 	buf->b_p_swf = TRUE;	/* may create a swap file */
+#ifdef FEAT_CRYPT
+	buf->b_p_key = empty_option;
+#endif
 	ml_open(buf);
 	ml_open_file(buf);	/* create swap file now */
     }
