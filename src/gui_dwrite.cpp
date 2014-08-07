0106b4b89127b043eddf711c750364b487deb794
Bram Moolenaar <Bram@vim.org>
2014-08-07 13:55:10 +0200

    updated for version 7.4.398 Problem:    Gcc error for the argument of InterlockedIncrement() and             InterlockedDecrement(). (Axel Bender) Solution:   Remove "unsigned" from the cRefCount_ declaration.

--- src/gui_dwrite.cpp
+++ src/gui_dwrite.cpp
@@ -377,7 +377,7 @@ public:
     }
 
 private:
-    unsigned long cRefCount_;
+    long cRefCount_;
     IDWriteBitmapRenderTarget* pRenderTarget_;
     IDWriteRenderingParams* pRenderingParams_;
 };
