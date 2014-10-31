2f31e399780f434c9635a1da969020824440e03b
Bram Moolenaar <Bram@vim.org>
2014-10-31 19:20:36 +0100

    updated for version 7.4.492 Problem:    In Insert mode, after inserting a newline that inserts a comment             leader, CTRL-O moves to the right. (ZyX) Issue 57. Solution:   Correct the condition for moving the cursor back to the NUL.             (Christian Brabandt)

--- src/edit.c
+++ src/edit.c
@@ -6916,8 +6916,12 @@ stop_insert(end_insert_pos, esc, nomove)
 	    }
 	    if (curwin->w_cursor.lnum != tpos.lnum)
 		curwin->w_cursor = tpos;
-	    else if (cc != NUL)
-		++curwin->w_cursor.col;	/* put cursor back on the NUL */
+	    else
+	    {
+		tpos.col++;
+		if (cc != NUL && gchar_pos(&tpos) == NUL)
+		    ++curwin->w_cursor.col;	/* put cursor back on the NUL */
+	    }
 
 	    /* <C-S-Right> may have started Visual mode, adjust the position for
 	     * deleted characters. */
