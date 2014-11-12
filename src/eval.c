8a349ff944dc01ca0e52f10144d8660aab430c9c
Bram Moolenaar <Bram@vim.org>
2014-11-12 20:09:06 +0100

    updated for version 7.4.516 Problem:    Completing a function name containing a # does not work.  Issue             253. Solution:   Recognize the # character. (Christian Brabandt)

--- src/eval.c
+++ src/eval.c
@@ -3368,11 +3368,11 @@ set_context_for_expression(xp, arg, cmdidx)
 	    got_eq = TRUE;
 	    xp->xp_context = EXPAND_EXPRESSION;
 	}
-	else if (c == '<'
+	else if ((c == '<' || c == '#')
 		&& xp->xp_context == EXPAND_FUNCTIONS
 		&& vim_strchr(xp->xp_pattern, '(') == NULL)
 	{
-	    /* Function name can start with "<SNR>" */
+	    /* Function name can start with "<SNR>" and contain '#'. */
 	    break;
 	}
 	else if (cmdidx != CMD_let || got_eq)
