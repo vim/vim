46538ee46cb04ed47c546d92583d6eb023e51b32
Bram Moolenaar <Bram@vim.org>
2015-02-17 16:28:55 +0100

    updated for version 7.4.638 Problem:    Can't build with Lua 5.3 on Windows. Solution:   use luaL_optinteger() instead of LuaL_optlong(). (Ken Takata)

--- src/if_lua.c
+++ src/if_lua.c
@@ -774,7 +774,7 @@ luaV_list_insert (lua_State *L)
 {
     luaV_List *lis = luaV_checkudata(L, 1, LUAVIM_LIST);
     list_T *l = (list_T *) luaV_checkcache(L, (void *) *lis);
-    long pos = luaL_optlong(L, 3, 0);
+    long pos = (long) luaL_optinteger(L, 3, 0);
     listitem_T *li = NULL;
     typval_T v;
     if (l->lv_lock)
