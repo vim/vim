e1a61991d9b6fd5f65636d17583f93118268cda5
Bram Moolenaar <Bram@vim.org>
2015-12-03 21:02:27 +0100

    patch 7.4.958 Problem:    Vim checks if the directory "$TMPDIR" exists. Solution:   Do not check if the name starts with "$".

--- src/fileio.c
+++ src/fileio.c
@@ -7388,10 +7388,12 @@ vim_tempname(extra_char, keep)
 	    long	off;
 # endif
 
-	    /* expand $TMP, leave room for "/v1100000/999999999" */
+	    /* Expand $TMP, leave room for "/v1100000/999999999".
+	     * Skip the directory check if the expansion fails. */
 	    expand_env((char_u *)tempdirs[i], itmp, TEMPNAMELEN - 20);
-	    if (mch_isdir(itmp))		/* directory exists */
+	    if (itmp[0] != '$' && mch_isdir(itmp))
 	    {
+		/* directory exists */
 # ifdef __EMX__
 		/* If $TMP contains a forward slash (perhaps using bash or
 		 * tcsh), don't add a backslash, use a forward slash!
