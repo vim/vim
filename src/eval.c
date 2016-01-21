5f8a14b9dea094b8bbab94cfc1e8da8e633fbc01
Bram Moolenaar <Bram@vim.org>
2016-01-21 23:34:58 +0100

    patch 7.4.1151 Problem:    Missing change to eval.c Solution:   Also change feedkeys().

--- src/eval.c
+++ src/eval.c
@@ -10960,6 +10960,7 @@ f_feedkeys(argvars, rettv)
     char_u	*keys, *flags;
     char_u	nbuf[NUMBUFLEN];
     int		typed = FALSE;
+    int		execute = FALSE;
     char_u	*keys_esc;
 
     /* This is not allowed in the sandbox.  If the commands would still be
@@ -10982,6 +10983,7 @@ f_feedkeys(argvars, rettv)
 		    case 'm': remap = TRUE; break;
 		    case 't': typed = TRUE; break;
 		    case 'i': insert = TRUE; break;
+		    case 'x': execute = TRUE; break;
 		}
 	    }
 	}
@@ -10996,6 +10998,8 @@ f_feedkeys(argvars, rettv)
 	    vim_free(keys_esc);
 	    if (vgetc_busy)
 		typebuf_was_filled = TRUE;
+	    if (execute)
+		exec_normal(TRUE);
 	}
     }
 }
