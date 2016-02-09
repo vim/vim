f068dcafcfe0c8018e5a559c50769ca1364bd9a5
Bram Moolenaar <Bram@vim.org>
2016-02-09 21:24:46 +0100

    patch 7.4.1296 Problem:    Cursor changes column with up motion when the matchparen plugin             saves and restores the cursor position. (Martin Kunev) Solution:   Make sure curswant is updated before invoking the autocommand.

--- src/edit.c
+++ src/edit.c
@@ -1611,7 +1611,12 @@ ins_redraw(
 # endif
 # ifdef FEAT_AUTOCMD
 	if (has_cursormovedI())
+	{
+	    /* Make sure curswant is correct, an autocommand may call
+	     * getcurpos(). */
+	    update_curswant();
 	    apply_autocmds(EVENT_CURSORMOVEDI, NULL, NULL, FALSE, curbuf);
+	}
 # endif
 # ifdef FEAT_CONCEAL
 	if (curwin->w_p_cole > 0)
