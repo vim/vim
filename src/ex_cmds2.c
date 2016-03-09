af1a0e371e739f8dff337fd31da0ff8ffb347b43
Bram Moolenaar <Bram@vim.org>
2016-03-09 22:19:26 +0100

    patch 7.4.1528 Problem:    Using "ever" for packages is confusing. Solution:   Use "start", as it's related to startup.

--- src/ex_cmds2.c
+++ src/ex_cmds2.c
@@ -3118,8 +3118,8 @@ add_pack_plugin(char_u *fname, void *cookie)
 	    }
 
 	/* now we have:
-	 * rtp/pack/name/ever/name
-	 *    p4   p3   p2   p1
+	 * rtp/pack/name/start/name
+	 *    p4   p3   p2    p1
 	 *
 	 * find the part up to "pack" in 'runtimepath' */
 	c = *p4;
@@ -3195,7 +3195,7 @@ theend:
     void
 source_packages()
 {
-    do_in_path(p_pp, (char_u *)"pack/*/ever/*", DIP_ALL + DIP_DIR,
+    do_in_path(p_pp, (char_u *)"pack/*/start/*", DIP_ALL + DIP_DIR,
 							add_pack_plugin, p_pp);
 }
 
