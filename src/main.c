baec5c1768098f9dd867b465aaabfdfb294f10c2
Bram Moolenaar <Bram@vim.org>
2016-04-06 23:06:23 +0200

    patch 7.4.1716 Problem:    'autochdir' doesn't work for the first file. (Rob Hoelz) Solution:   Call DO_AUTOCHDIR after startup. (Christian Brabandt, closes #704)

--- src/main.c
+++ src/main.c
@@ -959,6 +959,9 @@ vim_main2(int argc UNUSED, char **argv UNUSED)
     no_wait_return = FALSE;
     starting = 0;
 
+    /* 'autochdir' has been postponed */
+    DO_AUTOCHDIR
+
 #ifdef FEAT_TERMRESPONSE
     /* Requesting the termresponse is postponed until here, so that a "-c q"
      * argument doesn't make it appear in the shell Vim was started from. */
