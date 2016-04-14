baa9fcaf4042a6dbe01e64ce63cb54941ee00f09
Bram Moolenaar <Bram@vim.org>
2016-04-14 17:40:56 +0200

    patch 7.4.1736 Problem:    Unused variable. Solution:   Remove it. (Yasuhiro Matsumoto)

--- src/if_py_both.h
+++ src/if_py_both.h
@@ -3006,7 +3006,6 @@ FunctionAttr(FunctionObject *self, char *name)
     static void
 set_partial(FunctionObject *self, partial_T *pt, int exported)
 {
-    typval_T *curtv;
     int i;
 
     pt->pt_name = self->name;
--- src/version.c
+++ src/version.c
@@ -749,6 +749,8 @@ static char *(features[]) =
 static int included_patches[] =
 {   /* Add new patch number below this line */
 /**/
+    1736,
+/**/
     1735,
 /**/
     1734,
