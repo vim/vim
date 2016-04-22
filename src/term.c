a1c487eef71d1673e57511453009de9cb4c9af51
Bram Moolenaar <Bram@vim.org>
2016-04-22 20:20:19 +0200

    patch 7.4.1776 Problem:    Using wrong buffer length. Solution:   use the right name. (Kazunobu Kuriyama)

--- src/term.c
+++ src/term.c
@@ -2804,7 +2804,7 @@ term_rgb_color(char_u *s, long_u rgb)
 #define MAX_COLOR_STR_LEN 100
     char	buf[MAX_COLOR_STR_LEN];
 
-    vim_snprintf(buf, MAX_KEY_CODE_LEN,
+    vim_snprintf(buf, MAX_COLOR_STR_LEN,
 				  (char *)s, RED(rgb), GREEN(rgb), BLUE(rgb));
     OUT_STR(buf);
 }
