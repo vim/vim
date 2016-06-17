d18f672fc9477f3c0cb7cc4ce8d9237ed825c612
Bram Moolenaar <Bram@vim.org>
2016-06-17 13:18:49 +0200

    patch 7.4.1942 Problem:    Background is not drawn properly when 'termguicolors' is set. Solution:   Check cterm_normal_bg_color. (Jacob Niehus, closes #805)

--- src/screen.c
+++ src/screen.c
@@ -8967,9 +8967,12 @@ can_clear(char_u *p)
 		|| gui.in_use
 #endif
 #ifdef FEAT_TERMGUICOLORS
-		|| (p_tgc && cterm_normal_bg_gui_color != (long_u)INVALCOLOR)
+		|| (p_tgc && cterm_normal_bg_gui_color == (long_u)INVALCOLOR)
+		|| (!p_tgc && cterm_normal_bg_color == 0)
+#else
+		|| cterm_normal_bg_color == 0
 #endif
-		|| cterm_normal_bg_color == 0 || *T_UT != NUL));
+		|| *T_UT != NUL));
 }
 
 /*
