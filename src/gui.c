45a249513f5ed2de0a6f6b5cb6e32015e35644be
Bram Moolenaar <Bram@vim.org>
2016-07-24 22:25:15 +0200

    patch 7.4.2102 Problem:    Tiny build with GUI fails. Solution:   Revert one FOR_ALL_ change.

--- src/gui.c
+++ src/gui.c
@@ -4166,7 +4166,7 @@ gui_update_scrollbars(
     /* avoid that moving components around generates events */
     ++hold_gui_events;
 
-    FOR_ALL_WINDOWS(wp)
+    for (wp = firstwin; wp != NULL; wp = W_NEXT(wp))
     {
 	if (wp->w_buffer == NULL)	/* just in case */
 	    continue;
