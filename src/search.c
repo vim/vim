779f2fc3a7468e273897d2fd0672315812a2e3da
Bram Moolenaar <Bram@vim.org>
2016-09-01 20:58:24 +0200

    patch 7.4.2303 Problem:    When using "is" the mode isn't always updated. Solution:   Redraw the command line. (Christian Brabandt)

--- src/search.c
+++ src/search.c
@@ -3638,6 +3638,7 @@ extend:
 	    ++curwin->w_cursor.col;
 	VIsual = start_pos;
 	VIsual_mode = 'v';
+	redraw_cmdline = TRUE;		/* show mode later */
 	redraw_curbuf_later(INVERTED);	/* update the inversion */
     }
     else
