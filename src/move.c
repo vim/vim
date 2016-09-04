bc54f3f3fed4dc3556df8c46cee6739d211b0eb2
Bram Moolenaar <Bram@vim.org>
2016-09-04 14:34:28 +0200

    patch 7.4.2322 Problem:    Access memory beyond the end of the line. (Dominique Pelle) Solution:   Adjust the cursor column.

--- src/move.c
+++ src/move.c
@@ -2479,6 +2479,7 @@ onepage(int dir, long count)
     foldAdjustCursor();
 #endif
     cursor_correct();
+    check_cursor_col();
     if (retval == OK)
 	beginline(BL_SOL | BL_FIX);
     curwin->w_valid &= ~(VALID_WCOL|VALID_WROW|VALID_VIRTCOL);
