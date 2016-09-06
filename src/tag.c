72b4b870fcc445c14faf282e0595b5f9406b101d
Bram Moolenaar <Bram@vim.org>
2016-09-06 20:24:50 +0200

    patch 7.4.2335 Problem:    taglist() is slow. (Luc Hermitte) Solution:   Check for CTRL-C less often when doing a linear search. (closes             #1044)

--- src/tag.c
+++ src/tag.c
@@ -1578,7 +1578,11 @@ find_tags(
 	 */
 	for (;;)
 	{
-	    line_breakcheck();	    /* check for CTRL-C typed */
+	    /* check for CTRL-C typed, more often when jumping around */
+	    if (state == TS_BINARY || state == TS_SKIP_BACK)
+		line_breakcheck();
+	    else
+		fast_breakcheck();
 #ifdef FEAT_INS_EXPAND
 	    if ((flags & TAG_INS_COMP))	/* Double brackets for gcc */
 		ins_compl_check_keys(30);
