0ba407012c63064f03f1a5677677d4da423e5a73
Bram Moolenaar <Bram@vim.org>
2016-10-12 14:50:54 +0200

    patch 8.0.0030 Problem:    Mouse mode is not automatically detected for tmux. Solution:   Check for 'term' to be "tmux". (Michael Henry)

--- src/os_unix.c
+++ src/os_unix.c
@@ -2261,6 +2261,7 @@ use_xterm_like_mouse(char_u *name)
     return (name != NULL
 	    && (term_is_xterm
 		|| STRNICMP(name, "screen", 6) == 0
+		|| STRNICMP(name, "tmux", 4) == 0
 		|| STRICMP(name, "st") == 0
 		|| STRNICMP(name, "st-", 3) == 0
 		|| STRNICMP(name, "stterm", 6) == 0));
@@ -2324,6 +2325,7 @@ vim_is_fastterm(char_u *name)
     return (   STRNICMP(name, "hpterm", 6) == 0
 	    || STRNICMP(name, "sun-cmd", 7) == 0
 	    || STRNICMP(name, "screen", 6) == 0
+	    || STRNICMP(name, "tmux", 4) == 0
 	    || STRNICMP(name, "dtterm", 6) == 0);
 }
 
