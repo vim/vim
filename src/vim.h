ca291aec99b60fe81eaab36aa718e51421bb88d5
Bram Moolenaar <Bram@vim.org>
2016-10-15 19:33:50 +0200

    patch 8.0.0038 Problem:    OPEN_CHR_FILES not defined for FreeBSD using Debian userland             files. Solution:   Check for __FreeBSD_kernel__. (James McCoy, closes #1166)

--- src/vim.h
+++ src/vim.h
@@ -2495,7 +2495,8 @@ typedef enum
 #define FNE_INCL_BR	1	/* include [] in name */
 #define FNE_CHECK_START	2	/* check name starts with valid character */
 
-#if (defined(sun) || defined(__FreeBSD__)) && defined(S_ISCHR)
+#if (defined(sun) || defined(__FreeBSD__) || defined(__FreeBSD_kernel__)) \
+	&& defined(S_ISCHR)
 # define OPEN_CHR_FILES
 #endif
 
