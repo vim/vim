5198930b25b2561d07912c2d3b53b03cee378201
HuanGong <gonghuan.dev@gmail.com>
2016-12-22 16:43:41 +0800

    add compile script

---
+++ compile_with_gui.sh
@@ -0,0 +1,9 @@
+./configure --prefix=$HOME --with-features=huge \
+	--enable-multibyte \
+	--enable-pythoninterp=yes \
+	--with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
+	--enable-python3interp=yes \
+	--with-python3-config-dir=/usr/lib/python3.4/config-3.4m-x86_64-linux-gnu \
+	--enable-perlinterp=yes \
+	--enable-luainterp=yes \
+	--enable-cscope 
