a37ffaa5e0a47e2db27bc0cc23f49e7094f47f3b
Bram Moolenaar <Bram@vim.org>
2017-03-21 21:58:00 +0100

    patch 8.0.0502: Coverity complains about possible NULL pointer
    Problem:    Coverity complains about possible NULL pointer.
    Solution:   Add an assert(), let's see if this works on all systems.

--- src/window.c
+++ src/window.c
@@ -3806,6 +3806,7 @@ close_tabpage(tabpage_T *tab)
 	for (ptp = first_tabpage; ptp != NULL && ptp->tp_next != tab;
 							    ptp = ptp->tp_next)
 	    ;
+	assert(ptp != NULL);
 	ptp->tp_next = tab->tp_next;
     }
 
