e4a032dfce64fb18820671604a1cc53a05dfee0e
Dominique Pelle <dominique.pelle@gmail.com>
2017-05-08 20:58:21 +0200

    Changed the fix for memory error with :diffsplit :diffoff.
    When entering :diffsplit followed by :diffoff, w_p_fdm_save is empty
    and it is best to restore 'fdm' to "manual" rather than leaving set
    to 'diff'.

--- src/diff.c
+++ src/diff.c
@@ -1243,11 +1243,9 @@ ex_diffoff(exarg_T *eap)
 		if (!wp->w_p_wrap)
 		    wp->w_p_wrap = wp->w_p_wrap_save;
 #ifdef FEAT_FOLDING
-		if (*wp->w_p_fdm_save)
-		{
-		    free_string_option(wp->w_p_fdm);
-		    wp->w_p_fdm = vim_strsave(wp->w_p_fdm_save);
-		}
+		free_string_option(wp->w_p_fdm);
+		wp->w_p_fdm = wp->w_p_fdm_savevim_strsave(
+		    *wp->w_p_fdm_save ? wp->w_p_fdm_save : (char_u*)"manual");
 
 		if (wp->w_p_fdc == diff_foldcolumn)
 		    wp->w_p_fdc = wp->w_p_fdc_save;
