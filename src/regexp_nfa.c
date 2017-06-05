30fa923cefc1d69704457488a4a38991c4370d26
Bram Moolenaar <Bram@vim.org>
2017-06-05 13:30:06 +0200

    patch 8.0.0618: NFA regex engine handles [0-z] incorrectly
    Problem:    NFA regex engine handles [0-z] incorrectly.
    Solution:   Return at the right point. (James McCoy, closes #1703)

--- src/regexp_nfa.c
+++ src/regexp_nfa.c
@@ -628,37 +628,39 @@ nfa_recognize_char_class(char_u *start, char_u *end, int extra_newl)
 			config |= CLASS_o9;
 			break;
 		    }
-		    else
 		    if (*(p + 2) == '7')
 		    {
 			config |= CLASS_o7;
 			break;
 		    }
+		    return FAIL;
+
 		case 'a':
 		    if (*(p + 2) == 'z')
 		    {
 			config |= CLASS_az;
 			break;
 		    }
-		    else
 		    if (*(p + 2) == 'f')
 		    {
 			config |= CLASS_af;
 			break;
 		    }
+		    return FAIL;
+
 		case 'A':
 		    if (*(p + 2) == 'Z')
 		    {
 			config |= CLASS_AZ;
 			break;
 		    }
-		    else
 		    if (*(p + 2) == 'F')
 		    {
 			config |= CLASS_AF;
 			break;
 		    }
-		/* FALLTHROUGH */
+		    return FAIL;
+
 		default:
 		    return FAIL;
 	    }
