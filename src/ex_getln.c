71200225ee26b5e1e9ddeeaae779a2a780e845ab
Bram Moolenaar <Bram@vim.org>
2017-06-29 22:23:06 +0200

    patch 8.0.0689: ~ character not escaped when extending search pattern
    Problem:    The ~ character is not escaped when adding to the search pattern
                with CTRL-L. (Ramel Eshed)
    Solution:   Escape the character. (Christian Brabandt)

--- src/ex_getln.c
+++ src/ex_getln.c
@@ -1492,7 +1492,7 @@ getcmdline(
 			    if (c != NUL)
 			    {
 				if (c == firstc || vim_strchr((char_u *)(
-					      p_magic ? "\\^$.*[" : "\\^$"), c)
+					      p_magic ? "\\~^$.*[" : "\\^$"), c)
 								       != NULL)
 				{
 				    /* put a backslash before special
