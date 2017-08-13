69905d108be4af86c2d2a9224e2c588723276dc9
Bram Moolenaar <Bram@vim.org>
2017-08-13 18:14:47 +0200

    patch 8.0.0931: getwininfo() does not indicate a terminal window
    Problem:    getwininfo() does not indicate a terminal window.
    Solution:   Add "terminal" to the dictionary.

--- src/evalfunc.c
+++ src/evalfunc.c
@@ -5228,6 +5228,9 @@ get_win_info(win_T *wp, short tpnr, short winnr)
     dict_add_nr_str(dict, "width", wp->w_width, NULL);
     dict_add_nr_str(dict, "bufnr", wp->w_buffer->b_fnum, NULL);
 
+#ifdef FEAT_TERMINAL
+    dict_add_nr_str(dict, "terminal", bt_terminal(wp->w_buffer), NULL);
+#endif
 #ifdef FEAT_QUICKFIX
     dict_add_nr_str(dict, "quickfix", bt_quickfix(wp->w_buffer), NULL);
     dict_add_nr_str(dict, "loclist",
