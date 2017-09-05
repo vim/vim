c3f81394eff2b3edc7ea08405743f0d32048374a
Bram Moolenaar <Bram@vim.org>
2017-09-05 23:29:34 +0200

    patch 8.0.1062: Coverity warnings in libvterm
    Problem:    Coverity warnings in libvterm.
    Solution:   Add (void) to avoid warning for not checking return value.
                Add "break" before "case".

--- src/libvterm/src/screen.c
+++ src/libvterm/src/screen.c
@@ -214,7 +214,7 @@ static int moverect_internal(VTermRect dest, VTermRect src, void *user)
     VTermPos pos;
     for(pos.row = 0; pos.row < src.start_row; pos.row++) {
       for(pos.col = 0; pos.col < screen->cols; pos.col++)
-        vterm_screen_get_cell(screen, pos, screen->sb_buffer + pos.col);
+        (void)vterm_screen_get_cell(screen, pos, screen->sb_buffer + pos.col);
 
       (screen->callbacks->sb_pushline)(screen->cols, screen->sb_buffer, screen->cbdata);
     }
