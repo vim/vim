f5765d82d05195c4a800cd1e19ef97edea8b7c41
Jason Franklin <j_fra@fastmail.us>
2017-09-29 13:28:57 -0400

    Add assignment operations in "win_remove()"
    The "win_remove()" function in "window.c" did not simultaneously set
    the "firstwin" and the "curtab->tp_firstwin" pointers.  This meant
    that there was a chance of them being momentarily out of sync.
    
    For example, "gettabvar()" checks that the first window in the
    referenced tab is valid before getting the requested variable.  If
    "gettabvar()" is called from the tabline after the first window is
    closed (which calls "win_remove()" indirectly), the "firstwin" and
    the "curtab->tp_firstwin" pointers will differ!
    
    The above will cause "gettabvar()" to fail unexpectedly.

--- src/window.c
+++ src/window.c
@@ -4775,13 +4775,14 @@ win_remove(
     if (wp->w_prev != NULL)
 	wp->w_prev->w_next = wp->w_next;
     else if (tp == NULL)
-	firstwin = wp->w_next;
+	firstwin = curtab->tp_firstwin = wp->w_next;
     else
 	tp->tp_firstwin = wp->w_next;
+
     if (wp->w_next != NULL)
 	wp->w_next->w_prev = wp->w_prev;
     else if (tp == NULL)
-	lastwin = wp->w_prev;
+	lastwin = curtab->tp_lastwin = wp->w_prev;
     else
 	tp->tp_lastwin = wp->w_prev;
 }
