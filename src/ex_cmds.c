059db5c29ffef283a4b90bab9228708fa32e3dd2
Bram Moolenaar <Bram@vim.org>
2017-10-15 22:42:23 +0200

    patch 8.0.1202: :wall gives an errof for a terminal window
    Problem:    :wall gives an errof for a terminal window. (Marius Gedminas)
    Solution:   Don't try writing a buffer that can't be written. (Yasuhiro
                Matsumoto, closes #2190)

--- src/ex_cmds.c
+++ src/ex_cmds.c
@@ -3400,7 +3400,7 @@ do_wqall(exarg_T *eap)
 
     FOR_ALL_BUFFERS(buf)
     {
-	if (bufIsChanged(buf))
+	if (bufIsChanged(buf) && !bt_dontwrite(buf))
 	{
 	    /*
 	     * Check if there is a reason the buffer cannot be written:
