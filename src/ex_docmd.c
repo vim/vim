921e7e776cedda29c701b54bac2b25ac6f68592a
Bram Moolenaar <Bram@vim.org>
2017-10-19 12:37:42 +0200

    patch 8.0.1204: a QuitPre autocommand may get the wrong file name
    Problem:    A QuitPre autocommand may get the wrong file name.
    Solution:   Pass the buffer being closed to apply_autocmds(). (Rich Howe)

--- src/ex_docmd.c
+++ src/ex_docmd.c
@@ -7224,10 +7224,14 @@ ex_quit(exarg_T *eap)
 	wp = curwin;
 
 #ifdef FEAT_AUTOCMD
-    apply_autocmds(EVENT_QUITPRE, NULL, NULL, FALSE, curbuf);
-    /* Refuse to quit when locked or when the buffer in the last window is
-     * being closed (can only happen in autocommands). */
-    if (curbuf_locked() || !win_valid(wp)
+    /* Refuse to quit when locked. */
+    if (curbuf_locked())
+	return;
+    apply_autocmds(EVENT_QUITPRE, NULL, NULL, FALSE, wp->w_buffer);
+    /* Bail out when autocommands closed the window.
+     * Refuse to quit when the buffer in the last window is being closed (can
+     * only happen in autocommands). */
+    if (!win_valid(wp)
 	    || (wp->w_buffer->b_nwindows == 1 && wp->w_buffer->b_locked > 0))
 	return;
 #endif
