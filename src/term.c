53ec79537a062b453ed72a4efcb4b1cd284fe9a6
Bram Moolenaar <Bram@vim.org>
2017-11-05 21:24:23 +0100

    patch 8.0.1272: warnings for unused variables in tiny build
    Problem:    Warnings for unused variables in tiny build.
    Solution:   Add #ifdef. (Dominique Pelle, closes #2288)

--- src/term.c
+++ src/term.c
@@ -4757,10 +4757,11 @@ check_termcode(
 			if (i - j >= 21 && STRNCMP(tp + j + 3, "rgb:", 4) == 0
 			    && tp[j + 11] == '/' && tp[j + 16] == '/')
 			{
+#ifdef FEAT_TERMINAL
 			    int rval = hexhex2nr(tp + j + 7);
 			    int gval = hexhex2nr(tp + j + 12);
 			    int bval = hexhex2nr(tp + j + 17);
-
+#endif
 			    if (is_bg)
 			    {
 				char *newval = (3 * '6' < tp[j+7] + tp[j+12]
