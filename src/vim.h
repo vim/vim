275744ebdb15c06b44973324a9f9e086a4aa1292
Yegappan Lakshmanan <yegappan@yahoo.com>
2017-11-25 16:26:51 -0800

    Track changes to a quickfix list using changedtick and add a new autocmd for quickfix list changes

--- src/vim.h
+++ src/vim.h
@@ -1340,6 +1340,7 @@ enum auto_event
     EVENT_CMDUNDEFINED,		/* command undefined */
     EVENT_OPTIONSET,		/* option was set */
     EVENT_TEXTYANKPOST,		/* after some text was yanked */
+    EVENT_QFLISTCHANGED,	/* after a quickfix list is changed */
     NUM_EVENTS			/* MUST be the last one */
 };
 
