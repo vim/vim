7217a76aff9b769393df857bfb81f337f47fc8d4
Christian Brabandt <cb@256bit.org>
2018-03-23 22:31:25 +0100

    redefine UNUSED if not supported
    include Vims auto/config.h  header and redefine UNUSED according to the
    HAVE_ATTRIBUTE_UNUSED property.

--- src/xdiff/xinclude.h
+++ src/xdiff/xinclude.h
@@ -20,11 +20,21 @@
  *
  */
 
-#if defined(_MSC_VER)
-# define UNUSED
-# define inline __inline
-#else
+/* defines HAVE_ATTRIBUTE_UNUSED */
+#ifdef HAVE_CONFIG_H
+# include "../auto/config.h"
+#endif
+
+/* Mark unused function arguments with UNUSED, so that gcc -Wunused-parameter
+ * can be used to check for mistakes. */
+#ifdef HAVE_ATTRIBUTE_UNUSED
 # define UNUSED __attribute__((unused))
+#else
+# define UNUSED
+#endif
+
+#if defined(_MSC_VER)
+# define inline __inline
 #endif
 
 #if !defined(XINCLUDE_H)
