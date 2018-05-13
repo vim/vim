f9b3c2f9cae1af1414b36e218ca7ebe9b9c4dc55
Yegappan Lakshmanan <yegappan@yahoo.com>
2018-05-13 00:56:00 -0700

    Use vim_snprintf instead of snprintf

--- src/quickfix.c
+++ src/quickfix.c
@@ -1515,7 +1515,7 @@ qf_cmdtitle(char_u *cmd)
 {
     static char_u qftitle_str[IOSIZE];
 
-    snprintf((char *)qftitle_str, IOSIZE, ":%s", (char *)cmd);
+    vim_snprintf((char *)qftitle_str, IOSIZE, ":%s", (char *)cmd);
     return qftitle_str;
 }
 
