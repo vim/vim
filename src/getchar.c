833093bfb0e4a7f89b5adc66babcfa8ac09cfda9
Bram Moolenaar <Bram@vim.org>
2018-05-23 21:53:52 +0200

    patch 8.1.0022: repeating put from expression register fails
    Problem:    Repeating put from expression register fails.
    Solution:   Re-evaluate the expression register. (Andy Massimino,
                closes #2945)

--- src/getchar.c
+++ src/getchar.c
@@ -844,6 +844,14 @@ start_redo(long count, int old_redo)
 	if (c >= '1' && c < '9')
 	    ++c;
 	add_char_buff(&readbuf2, c);
+
+	/* the expression register should be re-evaluated */
+	if (c == '=')
+	{
+	    add_char_buff(&readbuf2, CAR);
+	    cmd_silent = TRUE;
+	}
+
 	c = read_redo(FALSE, old_redo);
     }
 
