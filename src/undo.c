efc81331e7bceafa2d7bc452a98d6ec1943b4292
Bram Moolenaar <Bram@vim.org>
2018-07-13 16:31:19 +0200

    patch 8.1.0179: redundant condition for boundary check
    Problem:    Redundant condition for boundary check.
    Solution:   Remove the condition. (Dominique Pelle). Change FALSE to FAIL.

--- src/undo.c
+++ src/undo.c
@@ -266,10 +266,8 @@ u_save(linenr_T top, linenr_T bot)
     if (undo_off)
 	return OK;
 
-    if (top > curbuf->b_ml.ml_line_count
-	    || top >= bot
-	    || bot > curbuf->b_ml.ml_line_count + 1)
-	return FALSE;	/* rely on caller to do error messages */
+    if (top >= bot || bot > curbuf->b_ml.ml_line_count + 1)
+	return FAIL;	// rely on caller to give an error message
 
     if (top + 2 == bot)
 	u_saveline((linenr_T)(top + 1));
