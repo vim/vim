3a53592d7bac272f078c9e7722aed5d8b737a11d
Yegappan Lakshmanan <yegappan@yahoo.com>
2018-07-14 12:22:11 -0700

    Fix sync conflict

--- src/window.c
+++ src/window.c
@@ -4373,28 +4373,6 @@ win_goto_hor(
 	win_goto(win);
 }
 
-    void
-win_get_neighbors(tabpage_T *tp, win_T *wp, list_T *neighbors)
-{
-    win_T	*win;
-
-    /* North neighbor */
-    win = win_vert_neighbor(tp, wp, TRUE, 1);
-    list_append_number(neighbors, (win != NULL && win != wp) ? win->w_id : 0);
-
-    /* East neighbor */
-    win = win_horz_neighbor(tp, wp, FALSE, 1);
-    list_append_number(neighbors, (win != NULL && win != wp) ? win->w_id : 0);
-
-    /* South neighbor */
-    win = win_vert_neighbor(tp, wp, FALSE, 1);
-    list_append_number(neighbors, (win != NULL && win != wp) ? win->w_id : 0);
-
-    /* West neighbor */
-    win = win_horz_neighbor(tp, wp, TRUE, 1);
-    list_append_number(neighbors, (win != NULL && win != wp) ? win->w_id : 0);
-}
-
 #if defined(FEAT_EVAL) || defined(PROTO)
     void
 win_get_neighbors(tabpage_T *tp, win_T *wp, list_T *neighbors)
