64f410742f101d7d5ea9e65503e1e0019605eaa5
Bram Moolenaar <Bram@vim.org>
2018-10-15 22:51:50 +0200

    patch 8.1.0479: failure when setting 'varsofttabstop' to end in a comma
    Problem:    Failure when setting 'varsofttabstop' to end in a comma. (Ralf
                Schandl)
    Solution:   Reject value with trailing command.  Add test for invalid values
                (closes #3544)

--- src/option.c
+++ src/option.c
@@ -12786,17 +12786,18 @@ tabstop_set(char_u *var, int **array)
     int t;
     char_u *cp;
 
-    if ((!var[0] || (var[0] == '0' && !var[1])))
+    if (var[0] == NUL || (var[0] == '0' && var[1] == NUL))
     {
 	*array = NULL;
 	return TRUE;
     }
 
-    for (cp = var; *cp; ++cp)
+    for (cp = var; *cp != NUL; ++cp)
     {
-	if (cp == var || *(cp - 1) == ',')
+	if (cp == var || cp[-1] == ',')
 	{
 	    char_u *end;
+
 	    if (strtol((char *)cp, (char **)&end, 10) <= 0)
 	    {
 		if (cp != end)
@@ -12809,7 +12810,7 @@ tabstop_set(char_u *var, int **array)
 
 	if (VIM_ISDIGIT(*cp))
 	    continue;
-	if (*cp == ',' && cp > var && *(cp - 1) != ',')
+	if (cp[0] == ',' && cp > var && cp[-1] != ',' && cp[1] != NUL)
 	{
 	    ++valcount;
 	    continue;
@@ -12818,16 +12819,16 @@ tabstop_set(char_u *var, int **array)
 	return FALSE;
     }
 
-    *array = (int *) alloc((unsigned) ((valcount + 1) * sizeof(int)));
+    *array = (int *)alloc((unsigned) ((valcount + 1) * sizeof(int)));
     (*array)[0] = valcount;
 
     t = 1;
-    for (cp = var; *cp;)
+    for (cp = var; *cp != NUL;)
     {
 	(*array)[t++] = atoi((char *)cp);
-	while (*cp && *cp != ',')
+	while (*cp  != NUL && *cp != ',')
 	    ++cp;
-	if (*cp)
+	if (*cp != NUL)
 	    ++cp;
     }
 
