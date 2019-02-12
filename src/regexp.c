061f8e491e6257128e08096404644b56a0e91937
Dominique Pelle <dominique.pelle@gmail.com>
2019-02-12 07:20:45 +0100

    Reduced sizeof(regitem_T) from 48 to 40 bytes on x86_64

--- src/regexp.c
+++ src/regexp.c
@@ -3546,13 +3546,13 @@ typedef enum regstate_E
 typedef struct regitem_S
 {
     regstate_T	rs_state;	/* what we are doing, one of RS_ above */
+    short	rs_no;		/* submatch nr or BEHIND/NOBEHIND */
     char_u	*rs_scan;	/* current node in program */
     union
     {
 	save_se_T  sesave;
 	regsave_T  regsave;
     } rs_un;			/* room for saving rex.input */
-    short	rs_no;		/* submatch nr or BEHIND/NOBEHIND */
 } regitem_T;
 
 static regitem_T *regstack_push(regstate_T state, char_u *scan);
