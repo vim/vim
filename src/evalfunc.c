1eed532999adc6a2c902fa07d256be988520a995
Bram Moolenaar <Bram@vim.org>
2019-02-26 17:03:54 +0100

    patch 8.1.0983: checking __CYGWIN32__ unnecessarily
    Problem:    Checking __CYGWIN32__ unnecessarily.
    Solution:   Remove the checks. (Ken Takata)

--- src/evalfunc.c
+++ src/evalfunc.c
@@ -6155,7 +6155,7 @@ f_has(typval_T *argvars, typval_T *rettv)
 #ifdef MSWIN
 	"win32",
 #endif
-#if defined(UNIX) && (defined(__CYGWIN32__) || defined(__CYGWIN__))
+#if defined(UNIX) && defined(__CYGWIN__)
 	"win32unix",
 #endif
 #ifdef _WIN64
