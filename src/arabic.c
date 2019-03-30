bd9bf266fccbf7b7f09e476e09b61f0133e914db
Bram Moolenaar <Bram@vim.org>
2019-03-30 18:25:39 +0100

    patch 8.1.1085: compiler warning for possibly uninitialized variable
    Problem:    Compiler warning for possibly uninitialized variable. (Tony
                Mechelynck)
    Solution:   Make conditions more logical.

--- src/arabic.c
+++ src/arabic.c
@@ -363,18 +363,23 @@ arabic_shape(
 	int backward_combine = !prev_laa && can_join(prev_c, c);
 	int forward_combine = can_join(c, next_c);
 
-	if (backward_combine && forward_combine)
-	    curr_c = curr_a->medial;
-	if (backward_combine && !forward_combine)
-	    curr_c = curr_a->final;
-	if (!backward_combine && forward_combine)
-	    curr_c = curr_a->initial;
-	if (!backward_combine && !forward_combine)
-	    curr_c = curr_a->isolated;
+	if (backward_combine)
+	{
+	    if (forward_combine)
+		curr_c = curr_a->medial;
+	    else
+		curr_c = curr_a->final;
+	}
+	else
+	{
+	    if (forward_combine)
+		curr_c = curr_a->initial;
+	    else
+		curr_c = curr_a->isolated;
+	}
     }
 
-    // Sanity check -- curr_c should, in the future, never be 0.
-    // We should, in the future, insert a fatal error here.
+    // Character missing from the table means using original character.
     if (curr_c == NUL)
 	curr_c = c;
 
