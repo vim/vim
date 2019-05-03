8c75e76114c63256df546acf9a06247ea01cb919
K.Takata <kentkt@csc.jp>
2019-05-04 07:11:20 +0900

    Fix usercmd
    * Add missing `*` to `addr_type_arg`. Compared different types.
    * Remove needless spaces and parentheses.

--- src/usercmd.c
+++ src/usercmd.c
@@ -824,10 +824,10 @@ invalid_count:
 		emsg(_("E179: argument required for -addr"));
 		return FAIL;
 	    }
-	    if (parse_addr_type_arg(val, (int)vallen,  addr_type_arg) == FAIL)
+	    if (parse_addr_type_arg(val, (int)vallen, addr_type_arg) == FAIL)
 		return FAIL;
-	    if (addr_type_arg != ADDR_LINES)
-		*argt |= (ZEROR) ;
+	    if (*addr_type_arg != ADDR_LINES)
+		*argt |= ZEROR;
 	}
 	else
 	{
