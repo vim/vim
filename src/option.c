d51b36d2d7cc45cf8284d776b83288ac6f47591d
shtanton <charlie@shtanton.com>
2019-06-24 21:20:32 +0100

    Increases the max numberwidth to NUMBUFLEN-1

--- src/option.c
+++ src/option.c
@@ -9493,10 +9493,10 @@ set_num_option(
 	    errmsg = e_positive;
 	    curwin->w_p_nuw = 1;
 	}
-	if (curwin->w_p_nuw > 10)
+	if (curwin->w_p_nuw > NUMBUFLEN-1)
 	{
 	    errmsg = e_invarg;
-	    curwin->w_p_nuw = 10;
+	    curwin->w_p_nuw = NUMBUFLEN-1;
 	}
 	curwin->w_nrwidth_line_count = 0; /* trigger a redraw */
     }
