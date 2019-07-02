3b888f20c7d4045125babffce78ef73cdc9e46c9
Yegappan Lakshmanan <yegappan@yahoo.com>
2019-07-02 07:44:30 -0700

    Fix problem in displaying signs in the number column with width 1

--- src/option.c
+++ src/option.c
@@ -7459,6 +7459,12 @@ did_set_string_option(
     {
 	if (check_opt_strings(*varp, p_scl_values, FALSE) != OK)
 	    errmsg = e_invarg;
+	// When changing the 'signcolumn' to or from 'number', recompute the
+	// width of the number column if 'number' or 'relativenumber' is set.
+	if (((*oldval == 'n' && *(oldval + 1) == 'u')
+		|| (*curwin->w_p_scl == 'n' && *(curwin->w_p_scl + 1) =='u'))
+		&& (curwin->w_p_nu || curwin->w_p_rnu))
+	    curwin->w_nrwidth_line_count = 0;
     }
 #endif
 
--- src/screen.c
+++ src/screen.c
@@ -11333,6 +11333,12 @@ number_width(win_T *wp)
     if (n < wp->w_p_nuw - 1)
 	n = wp->w_p_nuw - 1;
 
+    // If 'signcolumn' is set to 'number' and there is a sign to display, then
+    // the minimal width for the number column is 2.
+    if ((*wp->w_p_scl == 'n' && *(wp->w_p_scl + 1) == 'u')
+	    && (wp->w_buffer->b_signlist != NULL) && n < 2)
+	n = 2;
+
     wp->w_nrwidth_width = n;
     wp->w_nuw_cached = wp->w_p_nuw;
     return n;
