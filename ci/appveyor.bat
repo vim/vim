7ce2aa01c2e78f762f68c1c8df64649d790365e2
Bram Moolenaar <Bram@vim.org>
2019-07-16 20:00:11 +0200

    patch 8.1.1701: Appveyor build with MSVC fails puts progress bar in log
    Problem:    Appveyor build with MSVC fails puts progress bar in log.
    Solution:   Adjust the sed command. (Ken Takata)

--- ci/appveyor.bat
+++ ci/appveyor.bat
@@ -22,19 +22,20 @@ if "%FEATURE%" == "HUGE" (
 )
 .\gvim -u NONE -c "redir @a | ver |0put a | wq" ver_ming.txt
 
+:: Filter out the progress bar from the build log
+sed -e "s/@<<$/@<< | sed -e 's#.*\\\\r.*##'/" Make_mvc.mak > Make_mvc2.mak
+
 echo "Building MSVC 64bit console Version"
-:: This doesn't work with @<<
-:: sed -e "s/\$(LINKARGS2)/\$(LINKARGS2) | sed -e 's#.*\\\\r.*##'/" Make_mvc.mak > Make_mvc2.mak
-nmake -f Make_mvc.mak CPU=AMD64 OLE=no GUI=no IME=yes ICONV=yes DEBUG=no FEATURES=%FEATURE% || exit 1
-nmake -f Make_mvc.mak clean
+nmake -f Make_mvc2.mak CPU=AMD64 OLE=no GUI=no IME=yes ICONV=yes DEBUG=no FEATURES=%FEATURE% || exit 1
+nmake -f Make_mvc2.mak clean
 
 :: build MSVC huge version with python and channel support
 :: GUI needs to be last, so that testing works
 echo "Building MSVC 64bit GUI Version"
 if "%FEATURE%" == "HUGE" (
-    nmake -f Make_mvc.mak DIRECTX=yes CPU=AMD64 CHANNEL=yes OLE=no GUI=yes IME=yes ICONV=yes DEBUG=no PYTHON_VER=27 DYNAMIC_PYTHON=yes PYTHON=C:\Python27-x64 PYTHON3_VER=35 DYNAMIC_PYTHON3=yes PYTHON3=C:\Python35-x64 FEATURES=%FEATURE% || exit 1
+    nmake -f Make_mvc2.mak DIRECTX=yes CPU=AMD64 CHANNEL=yes OLE=no GUI=yes IME=yes ICONV=yes DEBUG=no PYTHON_VER=27 DYNAMIC_PYTHON=yes PYTHON=C:\Python27-x64 PYTHON3_VER=35 DYNAMIC_PYTHON3=yes PYTHON3=C:\Python35-x64 FEATURES=%FEATURE% || exit 1
 ) ELSE (
-    nmake -f Make_mvc.mak CPU=AMD64 OLE=no GUI=yes IME=yes ICONV=yes DEBUG=no FEATURES=%FEATURE% || exit 1
+    nmake -f Make_mvc2.mak CPU=AMD64 OLE=no GUI=yes IME=yes ICONV=yes DEBUG=no FEATURES=%FEATURE% || exit 1
 )
 .\gvim -u NONE -c "redir @a | ver |0put a | wq" ver_msvc.txt
 
