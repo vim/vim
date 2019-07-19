06e6377009c5763639310fa3bf892dec27a63334
Bram Moolenaar <Bram@vim.org>
2019-07-19 23:04:34 +0200

    patch 8.1.1715: emoji characters are seen as word characters for spelling
    Problem:    Emoji characters are seen as word characters for spelling. (Gautam
                Iyer)
    Solution:   Exclude class 3 from word characters.

--- src/spell.c
+++ src/spell.c
@@ -3077,7 +3077,7 @@ spell_mb_isword_class(int cl, win_T *wp)
     if (wp->w_s->b_cjk)
 	/* East Asian characters are not considered word characters. */
 	return cl == 2 || cl == 0x2800;
-    return cl >= 2 && cl != 0x2070 && cl != 0x2080;
+    return cl >= 2 && cl != 0x2070 && cl != 0x2080 && cl != 3;
 }
 
 /*
