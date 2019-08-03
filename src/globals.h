6cd57d44669c02af9195f5601b882edd435b47e8
Bram Moolenaar <Bram@vim.org>
2019-08-03 23:08:14 +0200

    patch 8.1.1805: au_did_filetype is declared twice
    Problem:    Au_did_filetype is declared twice.
    Solution:   Remove it from autocmd.c. (closes #4767)

--- src/globals.h
+++ src/globals.h
@@ -424,10 +424,17 @@ EXTERN int	autocmd_no_leave INIT(= FALSE); // *Leave autocmds disabled
 
 EXTERN int	modified_was_set;		// did ":set modified"
 EXTERN int	did_filetype INIT(= FALSE);	// FileType event found
-EXTERN int	au_did_filetype INIT(= FALSE);
 EXTERN int	keep_filetype INIT(= FALSE);	// value for did_filetype when
 						// starting to execute
 						// autocommands
+
+// Set by the apply_autocmds_group function if the given event is equal to
+// EVENT_FILETYPE. Used by the readfile function in order to determine if
+// EVENT_BUFREADPOST triggered the EVENT_FILETYPE.
+//
+// Relying on this value requires one to reset it prior calling
+// apply_autocmds_group.
+EXTERN int	au_did_filetype INIT(= FALSE);
 
 // When deleting the current buffer, another one must be loaded.  If we know
 // which one is preferred, au_new_curbuf is set to it
