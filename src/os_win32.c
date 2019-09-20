e840696c7c25efc2ebe6581ce46561cb197e5fa6
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2019-09-20 16:38:46 +0900

    Fix :sh on Windows

--- src/os_win32.c
+++ src/os_win32.c
@@ -4657,12 +4657,16 @@ mch_call_shell(
     {
 	char_u	*cmdbase = cmd;
 
-	// Skip a leading quote and (.
-	while (*cmdbase == '"' || *cmdbase == '(')
-	    ++cmdbase;
+	if (cmd != NULL)
+	{
+	    // Skip a leading quote and (.
+	    while (*cmdbase == '"' || *cmdbase == '(')
+		++cmdbase;
+	}
 
 	// Check the command does not begin with "start "
-	if (STRNICMP(cmdbase, "start", 5) != 0 || !VIM_ISWHITE(cmdbase[5]))
+	if (cmd == NULL ||
+	    (STRNICMP(cmdbase, "start", 5) != 0 || !VIM_ISWHITE(cmdbase[5])))
 	{
 	    // Use a terminal window to run the command in.
 	    x = mch_call_shell_terminal(cmd, options);
