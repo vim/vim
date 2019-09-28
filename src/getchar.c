0f1c6708fdf17bb9c7305b8af5d12189956195b6
Bram Moolenaar <Bram@vim.org>
2019-09-28 15:24:00 +0200

    patch 8.1.2091: double free when memory allocation fails
    Problem:    Double free when memory allocation fails. (Zu-Ming Jiang)
    Solution:   Use VIM_CLEAR() instead of vim_free(). (closes #4991)

--- src/getchar.c
+++ src/getchar.c
@@ -1296,11 +1296,11 @@ free_typebuf(void)
     if (typebuf.tb_buf == typebuf_init)
 	internal_error("Free typebuf 1");
     else
-	vim_free(typebuf.tb_buf);
+	VIM_CLEAR(typebuf.tb_buf);
     if (typebuf.tb_noremap == noremapbuf_init)
 	internal_error("Free typebuf 2");
     else
-	vim_free(typebuf.tb_noremap);
+	VIM_CLEAR(typebuf.tb_noremap);
 }
 
 /*
