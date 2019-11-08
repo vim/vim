b86bb9c48364d1262936c109366f968c28ae8581
Prabir Shrestha <mail@prabir.me>
2019-11-08 10:59:27 +0900

    add __vimcall global function
    example:
      __vimcall('eval', ['1+1'])

--- src/if_ecmascript.c
+++ src/if_ecmascript.c
@@ -2,7 +2,7 @@
  *
  * VIM - Vi IMproved	by Bram Moolenaar
  *
- * ECMAScript interface by Prabir Shrestha
+ * ECMAScript interface by Bob Pepin and Prabir Shrestha
  *
  * Do ":help uganda"  in Vim to read copying and usage conditions.
  * Do ":help credits" in Vim to see a list of people who contributed.
@@ -13,6 +13,51 @@
 
 #include "duktape/duktape.h"
 
+typval_T vduk_get_typval(duk_context *ctx, duk_idx_t idx) {
+    typval_T tv;
+    tv.v_lock = VAR_FIXED;
+    if(duk_is_string(ctx, idx)) {
+	tv.v_type = VAR_STRING;
+	tv.vval.v_string = (char_u*)duk_get_string(ctx, idx);
+    } else if(duk_is_number(ctx, idx)) {
+	tv.v_type = VAR_NUMBER;
+	tv.vval.v_number = duk_get_int(ctx, idx);
+    } else {
+	tv.v_type = VAR_UNKNOWN;
+    }
+    return tv;
+}
+
+duk_ret_t vduk_vimcall_func(duk_context *ctx) {
+    char_u *name = (char_u*)duk_to_string(ctx, -2);
+    int argcount = duk_get_length(ctx, -1);
+    typval_T *argvars = (typval_T*)alloc(argcount * sizeof(typval_T));
+    for(duk_uarridx_t i=0; i < argcount; i++) {
+	duk_get_prop_index(ctx, -1, i);
+	argvars[i] = vduk_get_typval(ctx, -1);
+	duk_pop(ctx);
+    }
+    typval_T rettv;
+    rettv.v_type = VAR_NUMBER;
+    call_internal_func(name, argcount, argvars, &rettv);
+    duk_pop_2(ctx);
+    duk_ret_t retcount = 0;
+    switch(rettv.v_type) {
+    case VAR_STRING:
+	duk_push_string(ctx, (const char*)rettv.vval.v_string);
+	retcount = 1;
+	break;
+    case VAR_NUMBER:
+	duk_push_int(ctx, rettv.vval.v_number);
+	retcount = 1;
+	break;
+    default:
+	break;
+    }
+    clear_tv(&rettv);
+    return retcount;
+}
+
 duk_context *vduk_get_context() {
     static duk_context *ctx = NULL;
     if(ctx) return ctx;
@@ -20,12 +65,9 @@ duk_context *vduk_get_context() {
 
     duk_push_global_object(ctx);
 
-    duk_idx_t obj_idx_vim;
+    duk_push_c_lightfunc(ctx, vduk_vimcall_func, 2, 2, 0);
+    duk_put_prop_string(ctx, -2, "__vimcall");
 
-    /* js: vim = {} */
-    duk_push_string(ctx, "vim");
-    obj_idx_vim = duk_push_object(ctx);
-    duk_put_prop(ctx, 0);
     duk_pop(ctx);
 
     return ctx;
@@ -48,8 +90,6 @@ ex_ecmascript(exarg_T *eap)
     duk_eval_string(ctx, evalstr);
     if (duk_peval_string(ctx, evalstr) != 0) {
 	semsg("ecmascript: %s", duk_safe_to_string(ctx, -1));
-    } else {
-	smsg("ecmascript: %s", duk_safe_to_string(ctx, -1));
     }
     duk_pop(ctx);
     if (script != NULL) vim_free(script);
