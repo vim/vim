a38bd9d3b41307ad35f9da6d7c2c64f133091bdb
Pavol Juhas <pavol.juhas@gmail.com>
2019-12-26 17:33:08 -0800

    Support deletion of A-Z file marks from viminfo

--- src/mark.c
+++ src/mark.c
@@ -857,7 +857,7 @@ ex_delmarks(exarg_T *eap)
 			namedfm[n].fmark.fnum = 0;
 			VIM_CLEAR(namedfm[n].fname);
 #ifdef FEAT_VIMINFO
-			namedfm[n].time_set = 0;
+			namedfm[n].time_set = digit ? 0 : vim_time();
 #endif
 		    }
 		}
