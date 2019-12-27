653ea2cdcdb0d4cdd42dd3b8d1cdcf82476bce05
Dominique Pelle <dominique.pelle@gmail.com>
2019-12-27 15:23:45 +0100

    Fixed compilation error on Windows

--- src/ex_cmds.c
+++ src/ex_cmds.c
@@ -5341,12 +5341,13 @@ help_compare(const void *s1, const void *s2)
 {
     char    *p1;
     char    *p2;
+    int	    cmp;
 
     p1 = *(char **)s1 + strlen(*(char **)s1) + 1;
     p2 = *(char **)s2 + strlen(*(char **)s2) + 1;
 
     // Compare by help heuristic number first.
-    int cmp = strcmp(p1, p2);
+    cmp = strcmp(p1, p2);
     if (cmp != 0)
 	return cmp;
 
