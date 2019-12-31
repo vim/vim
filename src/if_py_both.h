556684ff71e044a642879d759373a7ebc5047fad
Bram Moolenaar <Bram@vim.org>
2019-12-31 21:59:01 +0100

    patch 8.2.0068: crash when using Python 3 with "utf32" encoding
    Problem:    Crash when using Python 3 with "utf32" encoding. (Dominique Pelle)
    Solution:   Use "utf-8" whenever enc_utf8 is set. (closes #5423)

--- src/if_py_both.h
+++ src/if_py_both.h
@@ -19,7 +19,7 @@ static char_u e_py_systemexit[]	= "E880: Can't handle SystemExit of %s exception
 typedef int Py_ssize_t;  // Python 2.4 and earlier don't have this type.
 #endif
 
-#define ENC_OPT ((char *)p_enc)
+#define ENC_OPT (enc_utf8 ? "utf-8" : (char *)p_enc)
 #define DOPY_FUNC "_vim_pydo"
 
 static const char *vim_special_path = "_vim_path_";
