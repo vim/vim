41323eebcdaf5e87251c643ccb0c70a62a83bda3
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2020-01-09 01:46:26 +0900

    print blob

--- src/eval.c
+++ src/eval.c
@@ -5682,6 +5682,7 @@ tv_stringify(typval_T *varp, char_u *buf)
 {
     if (varp->v_type == VAR_LIST
 	    || varp->v_type == VAR_DICT
+	    || varp->v_type == VAR_BLOB
 	    || varp->v_type == VAR_FUNC
 	    || varp->v_type == VAR_PARTIAL
 	    || varp->v_type == VAR_FLOAT)
