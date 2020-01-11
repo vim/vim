ece5f47bd9c0bb6bf7ee4bb9ec980c2e06c28623
tyru <tyru.exe@gmail.com>
2020-01-12 01:52:27 +0900

    fix old comment

--- src/ex_docmd.c
+++ src/ex_docmd.c
@@ -6472,9 +6472,10 @@ post_chdir(cdscope_T scope)
 
 /*
  * Change directory function used by :cd/:tcd/:lcd Ex commands and the
- * chdir() function. If 'winlocaldir' is TRUE, then changes the window-local
- * directory. If 'tablocaldir' is TRUE, then changes the tab-local directory.
- * Otherwise changes the global directory.
+ * chdir() function.
+ * scope == CDSCOPE_WINDOW: changes the window-local directory
+ * scope == CDSCOPE_TABPAGE: changes the tab-local directory
+ * Otherwise: changes the global directory
  * Returns TRUE if the directory is successfully changed.
  */
     int
