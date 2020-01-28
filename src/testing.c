272ca95fc3d21ae1e2626a7aec38a6990e88ad6b
Bram Moolenaar <Bram@vim.org>
2020-01-28 20:49:11 +0100

    patch 8.2.0163: test hangs on MS-Windows console
    Problem:    Test hangs on MS-Windows console.
    Solution:   use feedkeys() instead of test_feedinput(). (Ken Takata)

--- src/testing.c
+++ src/testing.c
@@ -640,6 +640,12 @@ f_test_feedinput(typval_T *argvars, typval_T *rettv UNUSED)
 #ifdef USE_INPUT_BUF
     char_u	*val = tv_get_string_chk(&argvars[0]);
 
+# ifdef VIMDLL
+    // this doesn't work in the console
+    if (!gui.in_use)
+	return;
+# endif
+
     if (val != NULL)
     {
 	trash_input_buf();
