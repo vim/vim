e7ddf4e3372fad375038ad8771c6d1a7df49c34c
Bram Moolenaar <Bram@vim.org>
2020-02-03 21:29:30 +0100

    patch 8.2.0202: when 'lazyredraw' is set the window title may not be updated
    Problem:    When 'lazyredraw' is set the window title may not be updated.
    Solution:   Set "do_redraw" before entering the main loop. (Jason Franklin)

--- src/main.c
+++ src/main.c
@@ -893,6 +893,10 @@ vim_main2(void)
     }
 #endif
 
+    // Redraw at least once, also when 'lazyredraw' is set, to make sure the
+    // window title gets updated.
+    do_redraw = TRUE;
+
     TIME_MSG("before starting main loop");
 
     /*
