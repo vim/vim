82f654e092ac5b86316bc1b30c0b07a849813186
Bram Moolenaar <Bram@vim.org>
2020-02-17 22:12:50 +0100

    patch 8.2.0271: the "num64" feature is available everywhere
    Problem:    The "num64" feature is available everywhere and building without
                it causes problems.
    Solution:   Graduage the "num64" feature. (James McCoy, closes #5650)

--- src/version.c
+++ src/version.c
@@ -436,11 +436,7 @@ static char *(features[]) =
 #else
 	"-netbeans_intg",
 #endif
-#ifdef FEAT_NUM64
 	"+num64",
-#else
-	"-num64",
-#endif
 #ifdef FEAT_GUI_MSWIN
 # ifdef FEAT_OLE
 	"+ole",
@@ -742,6 +738,8 @@ static char *(features[]) =
 
 static int included_patches[] =
 {   /* Add new patch number below this line */
+/**/
+    271,
 /**/
     270,
 /**/
