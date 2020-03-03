6d4fd69e4ab9b6e54f5549a768e8d99b8108408a
K.Takata <kentkt@csc.jp>
2020-03-03 21:41:59 +0900

    win32: vimdll: Fix feedkeys() on vim.exe
    8.2.0329 revealed that feedkeys() on vim.exe with VIMDLL has an issue with
    handling 0x9b bytes. (Related: https://github.com/vim/vim/pull/5716)
    
    This reverts 8.1.1358 and 8.1.1239, and modifies mch_inchar() to encode 0x9b
    bytes so that the keys can be handled in the same way as unix version.

--- src/getchar.c
+++ src/getchar.c
@@ -1623,7 +1623,7 @@ vgetc(void)
 	    // Get two extra bytes for special keys
 	    if (c == K_SPECIAL
 #ifdef FEAT_GUI
-		    || (gui.in_use && c == CSI)
+		    || (c == CSI)
 #endif
 	       )
 	    {
@@ -1678,23 +1678,19 @@ vgetc(void)
 		}
 #endif
 #ifdef FEAT_GUI
-		if (gui.in_use)
+		// Handle focus event here, so that the caller doesn't need to
+		// know about it.  Return K_IGNORE so that we loop once (needed
+		// if 'lazyredraw' is set).
+		if (c == K_FOCUSGAINED || c == K_FOCUSLOST)
 		{
-		    // Handle focus event here, so that the caller doesn't
-		    // need to know about it.  Return K_IGNORE so that we loop
-		    // once (needed if 'lazyredraw' is set).
-		    if (c == K_FOCUSGAINED || c == K_FOCUSLOST)
-		    {
-			ui_focus_change(c == K_FOCUSGAINED);
-			c = K_IGNORE;
-		    }
-
-		    // Translate K_CSI to CSI.  The special key is only used
-		    // to avoid it being recognized as the start of a special
-		    // key.
-		    if (c == K_CSI)
-			c = CSI;
+		    ui_focus_change(c == K_FOCUSGAINED);
+		    c = K_IGNORE;
 		}
+
+		// Translate K_CSI to CSI.  The special key is only used to
+		// avoid it being recognized as the start of a special key.
+		if (c == K_CSI)
+		    c = CSI;
 #endif
 	    }
 	    // a keypad or special function key was not mapped, use it like
@@ -1772,11 +1768,7 @@ vgetc(void)
 		    buf[i] = vgetorpeek(TRUE);
 		    if (buf[i] == K_SPECIAL
 #ifdef FEAT_GUI
-			    || (
-# ifdef VIMDLL
-				gui.in_use &&
-# endif
-				buf[i] == CSI)
+			    || (buf[i] == CSI)
 #endif
 			    )
 		    {
