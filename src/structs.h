b42b92aa0ac8f609e0e600fbfc365dfa3342d3a0
Maxr1998 <max.rumpf1998@gmail.com>
2020-03-22 23:57:09 +0100

    cindent: introduce cinoptions entry to treat pragmas like normal code
    The new entry P in 'cinoptions' allows pragma directives (e.g. for OpenMP)
    to be treated as normal code, and have them indented like the rest of the
    program.

--- src/structs.h
+++ src/structs.h
@@ -2732,6 +2732,7 @@ struct file_buffer
     int		b_ind_cpp_namespace;
     int		b_ind_if_for_while;
     int		b_ind_cpp_extern_c;
+    int		b_ind_pragma;
 #endif
 
     linenr_T	b_no_eol_lnum;	// non-zero lnum when last line of next binary
