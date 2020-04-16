a07b11eab098153a199826c52e61b2c0f9ebdaac
Christian Brabandt <cb@256bit.org>
2020-04-16 22:33:57 +0200

    Allow to set backspace to a new value 'nostop'
    This behaves like the start value, but does makes CTRL-W and CTRL-U not
    stop at the start of insert position.
    
    Also allow the numeric value 3, which basically behaves the same.
    
    Add a couple of tests and some documentation
    
    See https://groups.google.com/forum/#!topic/vim_use/tGICgxJmdf8

--- src/option.h
+++ src/option.h
@@ -344,9 +344,14 @@
 #define WIM_BUFLASTUSED	0x08
 
 // arguments for can_bs()
+// each defined char should be unique over all values
+// except for BS_START, that intentionally also matches BS_NOSTOP
+// because BS_NOSTOP behaves exactly the same except it
+// does not stop at the start of the insert point
 #define BS_INDENT	'i'	// "Indent"
-#define BS_EOL		'o'	// "eOl"
+#define BS_EOL		'l'	// "eoL"
 #define BS_START	's'	// "Start"
+#define BS_NOSTOP	'p'	// "nostoP
 
 // flags for the 'culopt' option
 #define CULOPT_LINE	0x01	// Highlight complete line
