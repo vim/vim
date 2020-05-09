497f76bfbf9d5fe951221573d23696bb9e512c0b
Bram Moolenaar <Bram@vim.org>
2020-05-09 16:44:22 +0200

    patch 8.2.0721: Vim9: leaking memory when skipping
    Problem:    Vim9: leaking memory when skipping.
    Solution:   Disable skipping in generate_ppconst().

--- src/vim9compile.c
+++ src/vim9compile.c
@@ -2417,11 +2417,14 @@ generate_ppconst(cctx_T *cctx, ppconst_T *ppconst)
 {
     int	    i;
     int	    ret = OK;
+    int	    save_skip = cctx->ctx_skip;
 
+    cctx->ctx_skip = FALSE;
     for (i = 0; i < ppconst->pp_used; ++i)
 	if (generate_tv_PUSH(cctx, &ppconst->pp_tv[i]) == FAIL)
 	    ret = FAIL;
     ppconst->pp_used = 0;
+    cctx->ctx_skip = save_skip;
     return ret;
 }
 
@@ -2754,6 +2757,7 @@ compile_call(
 	    ++ppconst->pp_used;
 	    return OK;
 	}
+	clear_tv(&argvars[0]);
     }
 
     if (generate_ppconst(cctx, ppconst) == FAIL)
