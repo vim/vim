a9d4b84d97fb74061eeb42c1433e111fb58825dc
Bram Moolenaar <Bram@vim.org>
2020-05-30 14:46:52 +0200

    patch 8.2.0844: text properties crossing lines not handled correctly
    Problem:    Text properties crossing lines not handled correctly.
    Solution:   When saving for undo include an extra line when needed and do not
                adjust properties when undoing. (Axel Forsman, closes #5875)

--- src/structs.h
+++ src/structs.h
@@ -742,6 +742,15 @@ typedef struct memline
 #endif
 } memline_T;
 
+// Values for the flags argument of ml_delete_flags().
+#define ML_DEL_MESSAGE	    1	// may give a "No lines in buffer" message
+#define ML_DEL_UNDO	    2	// called from undo, do not update textprops
+
+// Values for the flags argument of ml_append_int().
+#define ML_APPEND_NEW	    1	// starting to edit a new file
+#define ML_APPEND_MARK	    2	// mark the new line
+#define ML_APPEND_UNDO	    4	// called from undo
+
 
 /*
  * Structure defining text properties.  These stick with the text.
