f9ab8ca66a67808c786a8082f638613a903fad35
ichizok <gclient.gaap@gmail.com>
2020-05-30 15:40:36 +0900

    Fix channel_open() in the environment without inet_ntop

--- src/channel.c
+++ src/channel.c
@@ -999,9 +999,10 @@ channel_open(
     for (addr = res; addr != NULL; addr = addr->ai_next)
     {
 	const char  *dst = hostname;
+	const void  *src UNUSED = NULL;
 # ifdef HAVE_INET_NTOP
-	const void  *src = NULL;
 	char	    buf[NUMBUFLEN];
+# endif
 
 	if (addr->ai_family == AF_INET6)
 	{
@@ -1017,6 +1018,7 @@ channel_open(
 	    sai->sin_port = htons(port);
 	    src = &sai->sin_addr;
 	}
+# ifdef HAVE_INET_NTOP
 	if (src != NULL)
 	{
 	    dst = inet_ntop(addr->ai_family, src, buf, sizeof(buf));
