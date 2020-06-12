722e505d1a55dfde5ab62241d10da91d2e10c3c1
Bram Moolenaar <Bram@vim.org>
2020-06-12 22:31:00 +0200

    patch 8.2.0966: 'shortmess' flag "n" not used in two places
    Problem:    'shortmess' flag "n" not used in two places.
    Solution:   Make use of the "n" flag consistent. (Nick Jensen, closes #6245,
                closes #6244)

--- src/buffer.c
+++ src/buffer.c
@@ -3651,7 +3651,7 @@ fileinfo(
 #ifdef FEAT_QUICKFIX
 		    && !bt_dontwrite(curbuf)
 #endif
-					? _("[New file]") : "",
+					   ? new_file_message() : "",
 	    (curbuf->b_flags & BF_READERR) ? _("[Read errors]") : "",
 	    curbuf->b_p_ro ? (shortmess(SHM_RO) ? _("[RO]")
 						      : _("[readonly]")) : "",
--- src/fileio.c
+++ src/fileio.c
@@ -506,7 +506,8 @@ readfile(
 			}
 		    }
 		    if (dir_of_file_exists(fname))
-			filemess(curbuf, sfname, (char_u *)_("[New File]"), 0);
+			filemess(curbuf, sfname,
+					      (char_u *)new_file_message(), 0);
 		    else
 			filemess(curbuf, sfname,
 					   (char_u *)_("[New DIRECTORY]"), 0);
