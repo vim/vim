02f9e6a60f9851046154ccf2ac170f25a0b3cc7d
Bram Moolenaar <Bram@vim.org>
2020-07-15 18:27:08 +0200

    patch 8.2.1221: memory leak when updating popup window
    Problem:    Memory leak when updating popup window.
    Solution:   Clear search highlighting.

--- src/popupwin.c
+++ src/popupwin.c
@@ -3855,6 +3855,11 @@ update_popups(void (*win_update)(win_T *wp))
 	// Back to the normal zindex.
 	screen_zindex = 0;
     }
+
+#if defined(FEAT_SEARCH_EXTRA)
+    // In case win_update() called start_search_hl().
+    end_search_hl();
+#endif
 }
 
 /*
