ecd34bf55d72f530859e290ac02ee41f9fb27cce
Bram Moolenaar <Bram@vim.org>
2020-08-04 20:17:31 +0200

    patch 8.2.1362: last entry of ":set term=xxx" overwritten by error message
    Problem:    Last entry of ":set term=xxx" overwritten by error message when
                'cmdheight' is two or more. (Tony Mechelynck)
    Solution:   Output extra line breaks.

--- src/term.c
+++ src/term.c
@@ -1763,6 +1763,7 @@ get_term_entries(int *height, int *width)
 report_term_error(char *error_msg, char_u *term)
 {
     struct builtin_term *termp;
+    int			i;
 
     mch_errmsg("\r\n");
     if (error_msg != NULL)
@@ -1787,6 +1788,10 @@ report_term_error(char *error_msg, char_u *term)
 	    mch_errmsg("\r\n");
 	}
     }
+    // Output extra 'cmdheight' line breaks to avoid that the following error
+    // message overwrites the last terminal name.
+    for (i = 1; i < p_ch; ++i)
+	mch_errmsg("\r\n");
 }
 
     static void
