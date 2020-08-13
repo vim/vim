bb88c344ab39ca7184301c11fbfa7b45623ad68c
ichizok <gclient.gaap@gmail.com>
2020-08-13 10:49:55 +0900

    [skip appveyor] CI: Reduce message output at installing package

--- ci/load-snd-dummy.sh
+++ ci/load-snd-dummy.sh
@@ -3,6 +3,6 @@ set -e
 
 if ! modprobe snd-dummy; then
     # snd-dummy is contained in linux-modules-extra (if exists)
-    apt install -y "linux-modules-extra-$(uname -r)"
+    apt-get install -yq --no-install-suggests --no-install-recommends "linux-modules-extra-$(uname -r)"
     modprobe snd-dummy
 fi
