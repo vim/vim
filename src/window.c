668008be66326ed9e5b1122abede34ed3d2de2d8
Bram Moolenaar <Bram@vim.org>
2020-10-01 19:06:35 +0200

    patch 8.2.1780: statusline not updated when splitting windows
    Problem:    Statusline not updated when splitting windows.
    Solution:   Call status_redraw_all(). (Jason Franklin, closes #5496)

--- src/window.c
+++ src/window.c
@@ -1273,13 +1273,11 @@ win_split_ins(
     if (flags & (WSP_TOP | WSP_BOT))
 	(void)win_comp_pos();
 
-    /*
-     * Both windows need redrawing
-     */
+     // Both windows need redrawing.  Update all status lines, in case they
+     // show something related to the window count or position.
     redraw_win_later(wp, NOT_VALID);
-    wp->w_redr_status = TRUE;
     redraw_win_later(oldwin, NOT_VALID);
-    oldwin->w_redr_status = TRUE;
+    status_redraw_all();
 
     if (need_status)
     {
