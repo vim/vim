a452b808b4da2d272ca4a50865eb8ca89a58f239
Bram Moolenaar <Bram@vim.org>
2020-12-01 21:47:59 +0100

    patch 8.2.2077: build failure with small features
    Problem:    Build failure with small features.
    Solution:   Add #ifdef.

--- src/ex_docmd.c
+++ src/ex_docmd.c
@@ -7994,12 +7994,14 @@ save_current_state(save_state_T *sst)
     sst->save_finish_op = finish_op;
     sst->save_opcount = opcount;
     sst->save_reg_executing = reg_executing;
-    sst->save_script_version = current_sctx.sc_version;
 
     msg_scroll = FALSE;		    // no msg scrolling in Normal mode
     restart_edit = 0;		    // don't go to Insert mode
     p_im = FALSE;		    // don't use 'insertmode'
+#ifdef FEAT_EVAL
+    sst->save_script_version = current_sctx.sc_version;
     current_sctx.sc_version = 1;    // not in Vim9 script
+#endif
 
     /*
      * Save the current typeahead.  This is required to allow using ":normal"
@@ -8023,7 +8025,9 @@ restore_current_state(save_state_T *sst)
     opcount = sst->save_opcount;
     reg_executing = sst->save_reg_executing;
     msg_didout |= sst->save_msg_didout;	// don't reset msg_didout now
+#ifdef FEAT_EVAL
     current_sctx.sc_version = sst->save_script_version;
+#endif
 
     // Restore the state (needed when called from a function executed for
     // 'indentexpr'). Update the mouse and cursor, they may have changed.
