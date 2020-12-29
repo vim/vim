89015a675990bd7d70e041c5d890edb803b5c6b7
Bram Moolenaar <Bram@vim.org>
2020-12-29 12:46:51 +0100

    patch 8.2.2244: crash when making the window width negative
    Problem:    Crash when making the window width of the not-current window
                negative.
    Solution:   Make sure the window width is not negative. (closes #7568)

--- src/version.c
+++ src/version.c
@@ -751,6 +751,8 @@ static char *(features[]) =
 static int included_patches[] =
 {   /* Add new patch number below this line */
 /**/
+    2244,
+/**/
     2243,
 /**/
     2242,
--- src/window.c
+++ src/window.c
@@ -5687,6 +5687,8 @@ win_setwidth_win(int width, win_T *wp)
 	if (width == 0)
 	    width = 1;
     }
+    else if (width < 0)
+	width = 0;
 
     frame_setwidth(wp->w_frame, width + wp->w_vsep_width);
 
