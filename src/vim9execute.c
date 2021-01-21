b1f2857096aabe9a11e576005d08fc4e53781698
Bram Moolenaar <Bram@vim.org>
2021-01-21 13:03:20 +0100

    patch 8.2.2382: build failure
    Problem:    Build failure.
    Solution:   Add missing changes.

--- src/vim9execute.c
+++ src/vim9execute.c
@@ -2951,17 +2951,19 @@ call_def_function(
 		    else
 #endif
 		    {
+			int failed = FALSE;
+
 			switch (iptr->isn_arg.op.op_type)
 			{
 			    case EXPR_MULT: n1 = n1 * n2; break;
-			    case EXPR_DIV:  n1 = num_divide(n1, n2);
-					    if (n2 == 0)
+			    case EXPR_DIV:  n1 = num_divide(n1, n2, &failed);
+					    if (failed)
 						goto on_error;
 					    break;
 			    case EXPR_SUB:  n1 = n1 - n2; break;
 			    case EXPR_ADD:  n1 = n1 + n2; break;
-			    default:	    n1 = num_modulus(n1, n2);
-					    if (n2 == 0)
+			    default:	    n1 = num_modulus(n1, n2, &failed);
+					    if (failed)
 						goto on_error;
 					    break;
 			}
