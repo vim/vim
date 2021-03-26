bb5d87c8504588be9c9d2fecc5b6455a2b2f6201
Bram Moolenaar <Bram@vim.org>
2021-03-26 22:15:26 +0100

    patch 8.2.2661: leaking memory when looping over a string
    Problem:    Leaking memory when looping over a string.
    Solution:   Free the memory.

--- src/eval.c
+++ src/eval.c
@@ -1815,8 +1815,10 @@ next_for_item(void *fi_void, char_u *arg)
 	tv.v_lock = VAR_FIXED;
 	tv.vval.v_string = vim_strnsave(fi->fi_string + fi->fi_byte_idx, len);
 	fi->fi_byte_idx += len;
-	return ex_let_vars(arg, &tv, TRUE, fi->fi_semicolon,
+	result = ex_let_vars(arg, &tv, TRUE, fi->fi_semicolon,
 					    fi->fi_varcount, flag, NULL) == OK;
+	vim_free(tv.vval.v_string);
+	return result;
     }
 
     item = fi->fi_lw.lw_item;
