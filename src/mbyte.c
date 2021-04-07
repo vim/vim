8dddc1f0e2e9eeb29fc59477b515bcd6bb1243ec
Bram Moolenaar <Bram@vim.org>
2021-04-07 19:00:25 +0200

    patch 8.2.2731: Mac: SF symbols are not displayed properly
    Problem:    Mac: SF symbols are not displayed properly.
    Solution:   Add custom range to list of double-width characters. (Yee Cheng
                Chin, closes #8077)

--- src/mbyte.c
+++ src/mbyte.c
@@ -1545,6 +1545,15 @@ utf_char2cells(int c)
 	{0x1f6e9, 0x1f6e9},
 	{0x1f6f0, 0x1f6f0},
 	{0x1f6f3, 0x1f6f3}
+
+#ifdef MACOS_X
+	// Include SF Symbols characters, which should be rendered as
+	// double-width. All of them are in the Supplementary Private Use
+	// Area-B range. The exact range was determined by downloading the "SF
+	// Symbols" app from Apple, and then selecting all symbols, copying
+	// them out, and inspecting the unicode values of them.
+	, {0x100000, 0x100d7f}
+#endif
     };
 
     if (c >= 0x100)
