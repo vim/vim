4eb1914e1bd870a7ea829cae150e1f9fdc3df7b1
matveyt <matthewtarasov@gmail.com>
2021-05-20 11:54:10 +0200

    patch 8.2.2874: MS-Windows: screen redraws too often
    Problem:    MS-Windows: screen redraws too often.
    Solution:   Do not redraw when peeking for a character. (closes #8230,
                closes #8211)

--- src/os_win32.c
+++ src/os_win32.c
@@ -1648,7 +1648,9 @@ WaitForChar(long msec, int ignore_input)
 	peek_console_input(g_hConIn, &ir, 1, &cRecords);
 
 # ifdef FEAT_MBYTE_IME
-	if (State & CMDLINE && msg_row == Rows - 1)
+	// May have to redraw if the cursor ends up in the wrong place.
+	// Only when not peeking.
+	if (State & CMDLINE && msg_row == Rows - 1 && msec != 0)
 	{
 	    CONSOLE_SCREEN_BUFFER_INFO csbi;
 
