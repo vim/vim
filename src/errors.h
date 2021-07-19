98c2eaed27579602e05e7b96aa3a60428a8b9cb2
Bram Moolenaar <Bram@vim.org>
2021-07-19 10:38:49 +0200

    patch 8.2.3183: duplicate error numbers
    Problem:    Duplicate error numbers.
    Solution:   Adjust the error numbers.

--- src/errors.h
+++ src/errors.h
@@ -505,6 +505,6 @@ EXTERN char e_number_required_for_argument_nr[]
 EXTERN char e_list_required_for_argument_nr[]
 	INIT(= N_("E1211: List required for argument %d"));
 EXTERN char e_bool_required_for_argument_nr[]
-	INIT(= N_("E1211: Bool required for argument %d"));
+	INIT(= N_("E1212: Bool required for argument %d"));
 EXTERN char e_redefining_imported_item_str[]
-	INIT(= N_("E1212: Redefining imported item %s"));
+	INIT(= N_("E1213: Redefining imported item %s"));
