94b6a6e3e98141ac529e0f174c942d19f7600fc3
Maxim Kim <habamax@gmail.com>
2021-07-20 10:51:52 +0300

    feat: use 'formatlistpat' length for breakindent
    Provided 'set briopt+=list:-1' use length of the matched 'formatlistpat'
    as additional indent to align contents of the list item.

--- src/indent.c
+++ src/indent.c
@@ -949,7 +949,7 @@ get_breakindent_win(
     bri += win_col_off2(wp);
 
     // add additional indent for numbered lists
-    if (wp->w_briopt_list > 0)
+    if (wp->w_briopt_list > 0 || wp->w_briopt_list == -1)
     {
 	regmatch_T	    regmatch;
 
@@ -958,7 +958,10 @@ get_breakindent_win(
 	if (regmatch.regprog != NULL)
 	{
 	    if (vim_regexec(&regmatch, line, 0))
-		bri += wp->w_briopt_list;
+		if (wp->w_briopt_list > 0)
+		    bri += wp->w_briopt_list;
+		else
+		    bri += (*regmatch.endp - *regmatch.startp);
 	    vim_regfree(regmatch.regprog);
 	}
     }
