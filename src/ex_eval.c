cce81e9673fe8d056e8eef310d9919620eccb2f2
Bram Moolenaar <Bram@vim.org>
2021-10-06 22:08:11 +0100

    patch 8.2.3486: illegal memory access with invalid sequence of commands
    Problem:    Illegal memory access with invalid sequence of commands.
    Solution:   Do not call leave_block() when not in a try block. (closes #8966)
                Reset did_emsg so that exception is shown as an error.

--- src/ex_eval.c
+++ src/ex_eval.c
@@ -1385,7 +1385,7 @@ ex_endwhile(exarg_T *eap)
 	eap->errmsg = _(err);
     else
     {
-	fl =  cstack->cs_flags[cstack->cs_idx];
+	fl = cstack->cs_flags[cstack->cs_idx];
 	if (!(fl & csf))
 	{
 	    // If we are in a ":while" or ":for" but used the wrong endloop
@@ -2007,6 +2007,7 @@ ex_endtry(exarg_T *eap)
 	if (!(cstack->cs_flags[cstack->cs_idx] & CSF_TRY))
 	{
 	    eap->errmsg = get_end_emsg(cstack);
+
 	    // Find the matching ":try" and report what's missing.
 	    idx = cstack->cs_idx;
 	    do
@@ -2025,6 +2026,9 @@ ex_endtry(exarg_T *eap)
 	     */
 	    if (did_throw)
 		discard_current_exception();
+
+	    // report eap->errmsg, also when there already was an error
+	    did_emsg = FALSE;
 	}
 	else
 	{
@@ -2105,7 +2109,9 @@ ex_endtry(exarg_T *eap)
 	 */
 	(void)cleanup_conditionals(cstack, CSF_TRY | CSF_SILENT, TRUE);
 
-	leave_block(cstack);
+	if (cstack->cs_idx >= 0
+			       && (cstack->cs_flags[cstack->cs_idx] & CSF_TRY))
+	    leave_block(cstack);
 	--cstack->cs_trylevel;
 
 	if (!skip)
