76d0992b6c03137bf53074b6d2996438d00899d3
naohiro ono <obcat@icloud.com>
2021-11-10 14:16:44 +0900

    Fix coding style

--- src/window.c
+++ src/window.c
@@ -2721,11 +2721,12 @@ win_close(win_T *win, int free_buf)
     static void
 trigger_winclosed(win_T *win)
 {
-    static int recursive = FALSE;
+    static int	recursive = FALSE;
+    char_u	winid[NUMBUFLEN];
+
     if (recursive)
 	return;
     recursive = TRUE;
-    char_u winid[NUMBUFLEN];
     vim_snprintf((char *)winid, sizeof(winid), "%i", win->w_id);
     apply_autocmds(EVENT_WINCLOSED, winid, winid, FALSE, win->w_buffer);
     recursive = FALSE;
