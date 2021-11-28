2e0f3ecb7046e6d7915327270f836c48ad033a67
Christian Brabandt <cb@256bit.org>
2021-11-28 18:41:05 +0000

    patch 8.2.3688: the window title is not updated when dragging the scrollbar
    Problem:    The window title is not updated when dragging the scrollbar.
    Solution:   Call maketitle(). (Christian Brabandt, closes #9238, closes #5383)

--- src/gui.c
+++ src/gui.c
@@ -4412,6 +4412,10 @@ gui_update_scrollbars(
 					    val, size, max);
 	}
     }
+
+    // update the title, it may show the scroll position
+    maketitle();
+
     prev_curwin = curwin;
     --hold_gui_events;
 }
