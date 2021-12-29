49a3fa209337b59efdbdc1c2dbcb640ae34da147
Yegappan Lakshmanan <yegappan@yahoo.com>
2021-12-28 19:55:16 -0800

    Fix tiny build failure

--- src/insexpand.c
+++ src/insexpand.c
@@ -3184,12 +3184,12 @@ get_next_cmdline_completion()
  * Get the next set of spell suggestions matching "compl_pattern".
  */
     static void
-get_next_spell_completion(linenr_T lnum)
+get_next_spell_completion(linenr_T lnum UNUSED)
 {
+#ifdef FEAT_SPELL
     char_u	**matches;
     int		num_matches;
 
-#ifdef FEAT_SPELL
     num_matches = expand_spelling(lnum, compl_pattern, &matches);
     if (num_matches > 0)
 	ins_compl_add_matches(num_matches, matches, p_ic);
