8e7cc6b920ddea37deaa5e6b7b3bdfff2222d137
Bram Moolenaar <Bram@vim.org>
2021-12-30 10:32:25 +0000

    patch 8.2.3942: Coverity reports a possible memory leak
    Problem:    Coverity reports a possible memory leak.
    Solution:   Free the array if allocation fails.

--- src/insexpand.c
+++ src/insexpand.c
@@ -3193,6 +3193,8 @@ get_next_spell_completion(linenr_T lnum UNUSED)
     num_matches = expand_spelling(lnum, compl_pattern, &matches);
     if (num_matches > 0)
 	ins_compl_add_matches(num_matches, matches, p_ic);
+    else
+	vim_free(matches);
 #endif
 }
 
