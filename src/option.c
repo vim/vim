9fdea5345e0618dd84fae45a21e85b7c846ca876
Christian Brabandt <cb@256bit.org>
2022-01-14 21:33:43 +0100

    breakindent: cache formatlistpattern
    Correctly initialize the value for the breakindent list suboption.
    
    Add some tests to verify the behaviour:
    - regex match of colummn is correct (test for #9309)
    - changing a line so that formatlistpat does no longer match
      -> no indent
    - changing 'formatlistpat' option -> adjust indent
    
    While at it: also mention in the documentation what internal value
    of 'breakindentopt'=min:<value>
    Because this helps to understand why breakindent sometimes does not kick in
    
    This fixes: #9512

--- src/option.c
+++ src/option.c
@@ -7053,6 +7053,18 @@ get_bkc_value(buf_T *buf)
 }
 
 /*
+ * Get the local or global value of 'formatlistpat'.
+ */
+    char_u *
+get_flp_value(buf_T *buf)
+{
+    return buf->b_p_flp ? buf->b_p_flp : p_flp;
+    if (buf->b_p_flp == NULL || *buf->b_p_flp == NUL)
+	return p_flp;
+    return buf->b_p_flp;
+}
+
+/*
  * Get the local or global value of the 'virtualedit' flags.
  */
     unsigned int
