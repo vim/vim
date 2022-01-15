e1833bfd01c100896d2a01f281762c285192d84b
zeertzjq <zeertzjq@outlook.com>
2022-01-15 10:52:16 +0000

    patch 8.2.4094: 'virtualedit' is window-local but using buffer-local enum
    Problem:    'virtualedit' is window-local but using buffer-local enum.
    Solution:   Use window-local enum. (closes #9529)

--- src/option.h
+++ src/option.h
@@ -1235,7 +1235,6 @@ enum
     , BV_VSTS
     , BV_VTS
 #endif
-    , BV_VE
     , BV_COUNT	    // must be the last one
 };
 
@@ -1287,6 +1286,7 @@ enum
 #endif
     , WV_NU
     , WV_RNU
+    , WV_VE
 #ifdef FEAT_LINEBREAK
     , WV_NUW
 #endif
