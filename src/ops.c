57df9e8a9f9ae1aafdde9b86b10ad907627a87dc
Bram Moolenaar <Bram@vim.org>
2022-01-20 12:10:48 +0000

    patch 8.2.4151: reading beyond the end of a line
    Problem:    Reading beyond the end of a line.
    Solution:   For block insert only use the offset for correcting the length.

--- src/ops.c
+++ src/ops.c
@@ -529,24 +529,9 @@ block_insert(
 	}
 
 	if (has_mbyte && spaces > 0)
-	{
-	    int off;
+	    // avoid copying part of a multi-byte character
+	    offset -= (*mb_head_off)(oldp, oldp + offset);
 
-	    // Avoid starting halfway a multi-byte character.
-	    if (b_insert)
-	    {
-		off = (*mb_head_off)(oldp, oldp + offset + spaces);
-		spaces -= off;
-		count -= off;
-	    }
-	    else
-	    {
-		// spaces fill the gap, the character that's at the edge moves
-		// right
-		off = (*mb_head_off)(oldp, oldp + offset);
-		offset -= off;
-	    }
-	}
 	if (spaces < 0)  // can happen when the cursor was moved
 	    spaces = 0;
 
--- src/version.c
+++ src/version.c
@@ -751,6 +751,8 @@ static char *(features[]) =
 static int included_patches[] =
 {   /* Add new patch number below this line */
 /**/
+    4151,
+/**/
     4150,
 /**/
     4149,
