4e3b3182307444f205a5f35e257c651f0f3717ad
Bram Moolenaar <Bram@vim.org>
2022-02-01 10:16:00 +0000

    patch 8.2.4278: build with Athena GUI fails
    Problem:    Build with Athena GUI fails. (Elimar Riesebieter)
    Solution:   Add #ifdef.

--- src/testing.c
+++ src/testing.c
@@ -1331,6 +1331,7 @@ test_gui_drop_files(dict_T *args UNUSED)
     return TRUE;
 }
 
+#if defined(FIND_REPLACE_DIALOG)
     static int
 test_gui_find_repl(dict_T *args)
 {
@@ -1357,6 +1358,7 @@ test_gui_find_repl(dict_T *args)
 
     return retval;
 }
+#endif
 
     static int
 test_gui_mouse_event(dict_T *args)
@@ -1476,8 +1478,10 @@ f_test_gui_event(typval_T *argvars UNUSED, typval_T *rettv UNUSED)
     event = tv_get_string(&argvars[0]);
     if (STRCMP(event, "dropfiles") == 0)
 	rettv->vval.v_number = test_gui_drop_files(argvars[1].vval.v_dict);
+#  if defined(FIND_REPLACE_DIALOG)
     else if (STRCMP(event, "findrepl") == 0)
 	rettv->vval.v_number = test_gui_find_repl(argvars[1].vval.v_dict);
+#  endif
     else if (STRCMP(event, "mouse") == 0)
 	rettv->vval.v_number = test_gui_mouse_event(argvars[1].vval.v_dict);
     else if (STRCMP(event, "scrollbar") == 0)
