d6211a52ab9f53b82f884561ed43d2fe4d24ff7d
Bram Moolenaar <Bram@vim.org>
2022-06-18 19:48:14 +0100

    patch 8.2.5126: substitute may overrun destination buffer
    Problem:    Substitute may overrun destination buffer.
    Solution:   Disallow switching buffers in a substitute expression.

--- src/ex_docmd.c
+++ src/ex_docmd.c
@@ -7138,9 +7138,10 @@ do_exedit(
 #endif
 	    )
     {
-	// Can't edit another file when "curbuf_lock" is set.  Only ":edit"
-	// can bring us here, others are stopped earlier.
-	if (*eap->arg != NUL && curbuf_locked())
+	// Can't edit another file when "textlock" or "curbuf_lock" is set.
+	// Only ":edit" or ":script" can bring us here, others are stopped
+	// earlier.
+	if (*eap->arg != NUL && text_or_buf_locked())
 	    return;
 
 	n = readonlymode;
--- src/version.c
+++ src/version.c
@@ -735,6 +735,8 @@ static char *(features[]) =
 static int included_patches[] =
 {   /* Add new patch number below this line */
 /**/
+    5126,
+/**/
     5125,
 /**/
     5124,
