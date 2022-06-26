0fbc9260a75dfc4d86f20e7c0eb76878f513a212
Bram Moolenaar <Bram@vim.org>
2022-06-26 11:17:10 +0100

    patch 8.2.5161: might still access invalid memory
    Problem:    Might still access invalid memory.
    Solution:   Add extra check for negative value.

--- src/message.c
+++ src/message.c
@@ -949,8 +949,10 @@ msg_may_trunc(int force, char_u *s)
     int		n;
     int		room;
 
+    // If something unexpected happened "room" may be negative, check for that
+    // just in case.
     room = (int)(Rows - cmdline_row - 1) * Columns + sc_col - 1;
-    if ((force || (shortmess(SHM_TRUNC) && !exmode_active))
+    if (room > 0 && (force || (shortmess(SHM_TRUNC) && !exmode_active))
 	    && (n = (int)STRLEN(s) - room) > 0)
     {
 	if (has_mbyte)
