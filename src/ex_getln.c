fbaf237320db2363eaa756b116c7732f534f5a2c
zeertzjq <zeertzjq@outlook.com>
2022-07-12 21:41:49 +0800

    Clear got_int before getting a character in Cmdline mode

--- src/ex_getln.c
+++ src/ex_getln.c
@@ -1786,6 +1786,8 @@ getcmdline_int(
 				// that occurs while typing a command should
 				// cause the command not to be executed.
 
+	got_int = FALSE;	// Avoid infinite Ctrl-C loop in Ex mode
+
 	// Trigger SafeState if nothing is pending.
 	may_trigger_safestate(xpc.xp_numfiles <= 0);
 
