b9e717367c395490149495cf375911b5d9de889e
Bram Moolenaar <Bram@vim.org>
2022-07-23 06:53:08 +0100

    patch 9.0.0060: accessing uninitialized memory when completing long line
    Problem:    Accessing uninitialized memory when completing long line.
    Solution:   Terminate string with NUL.

--- src/insexpand.c
+++ src/insexpand.c
@@ -642,6 +642,7 @@ ins_compl_infercase_gettext(
 	    // growarray.  Add the character in the next round.
 	    if (ga_grow(&gap, IOSIZE) == FAIL)
 		return (char_u *)"[failed]";
+	    *p = NUL;
 	    STRCPY(gap.ga_data, IObuff);
 	    gap.ga_len = (int)STRLEN(IObuff);
 	}
--- src/version.c
+++ src/version.c
@@ -736,6 +736,8 @@ static char *(features[]) =
 static int included_patches[] =
 {   /* Add new patch number below this line */
 /**/
+    60,
+/**/
     59,
 /**/
     58,
