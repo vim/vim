ecdc82e74e6a7e73d9067ece1d5eac33abfde5ed
zeertzjq <zeertzjq@outlook.com>
2022-07-25 19:50:57 +0100

    patch 9.0.0071: command overlaps with printed text in scrollback
    Problem:    Command overlaps with printed text in scrollback.
    Solution:   Clear until end-of-line and use correct message chunk.
                (closes #10765, closes #10764)

--- src/message.c
+++ src/message.c
@@ -2595,6 +2595,7 @@ sb_text_start_cmdline(void)
 sb_text_end_cmdline(void)
 {
     do_clear_sb_text = SB_CLEAR_CMDLINE_DONE;
+    msg_sb_eol();
 }
 
 /*
@@ -2614,7 +2615,7 @@ clear_sb_text(int all)
     {
 	if (last_msgchunk == NULL)
 	    return;
-	lastp = &last_msgchunk->sb_prev;
+	lastp = &msg_sb_start(last_msgchunk)->sb_prev;
     }
 
     while (*lastp != NULL)
