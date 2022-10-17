9652249a2d02318a28a63a7b5711f25652e8f969
Bram Moolenaar <Bram@vim.org>
2022-10-17 20:00:26 +0100

    patch 9.0.0785: memory leak with empty shell command
    Problem:    Memory leak with empty shell command.
    Solution:   Free the allocated memory when bailing out.

--- src/ex_cmds.c
+++ src/ex_cmds.c
@@ -960,7 +960,10 @@ do_bang(
     // Don't do anything if there is no command as there isn't really anything
     // useful in running "sh -c ''".  Avoids changing "prevcmd".
     if (STRLEN(newcmd) == 0)
+    {
+	vim_free(newcmd);
 	return;
+    }
 
     vim_free(prevcmd);
     prevcmd = newcmd;
