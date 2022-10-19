ff62e5ad2ee2dadbae190101185846e06ca120b5
K.Takata <kentkt@csc.jp>
2022-10-19 18:34:59 +0900

    Fix error

--- src/filepath.c
+++ src/filepath.c
@@ -2019,7 +2019,7 @@ read_file_or_blob(typval_T *argvars, typval_T *rettv, int always_blob)
 f_readblob(typval_T *argvars, typval_T *rettv)
 {
     if (in_vim9script()
-	    && (check_for_nonempty_string_arg(argvars, 0) == FAIL
+	    && (check_for_string_arg(argvars, 0) == FAIL
 		|| check_for_opt_number_arg(argvars, 1) == FAIL
 		|| (argvars[1].v_type != VAR_UNKNOWN
 		    && check_for_opt_number_arg(argvars, 2) == FAIL)))
