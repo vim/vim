dbe2aef1c87d474c6b393f4b2360195c48e185ed
Christopher Plewright <chris@createng.com>
2022-11-05 15:26:19 +1100

    MS-Win Cmd prompt with VTP enabled needs same rgb color flush as WT

--- src/term.c
+++ src/term.c
@@ -2987,7 +2987,7 @@ term_rgb_color(char_u *s, guicolor_T rgb)
     vim_snprintf(buf, MAX_COLOR_STR_LEN,
 				  (char *)s, RED(rgb), GREEN(rgb), BLUE(rgb));
 #ifdef FEAT_VTP
-    if (use_wt())
+    if (has_vtp_working())
     {
 	out_flush();
 	buf[1] = '[';
