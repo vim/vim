edade3db2d73c3fa9f87bdb290f451387a2bf076
zeertzjq <zeertzjq@outlook.com>
2022-12-19 17:53:02 +0800

    Fix foldmethod=indent uses wrong 'tabstop' after :diffoff!

--- src/indent.c
+++ src/indent.c
@@ -420,7 +420,7 @@ get_indent_buf(buf_T *buf, linenr_T lnum)
 {
 # ifdef FEAT_VARTABS
     return get_indent_str_vtab(ml_get_buf(buf, lnum, FALSE),
-			       (int)curbuf->b_p_ts, buf->b_p_vts_array, FALSE);
+			       (int)buf->b_p_ts, buf->b_p_vts_array, FALSE);
 # else
     return get_indent_str(ml_get_buf(buf, lnum, FALSE), (int)buf->b_p_ts, FALSE);
 # endif
