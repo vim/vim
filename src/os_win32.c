c84b3bafede0a590b39e63f6e33ccd41a911518e
Christopher Plewright <chris@createng.com>
2023-01-01 19:46:06 +1100

    special key modifiers

--- src/os_win32.c
+++ src/os_win32.c
@@ -1120,11 +1120,11 @@ decode_key_event(
 	{
 	    if (nModifs == 0)
 		*pch = VirtKeyMap[i].chAlone;
-	    else if ((nModifs & SHIFT) != 0 && (nModifs & ~SHIFT) == 0)
+	    else if ((nModifs & SHIFT) && (vtp_working || !(nModifs & ~SHIFT)))
 		*pch = VirtKeyMap[i].chShift;
-	    else if ((nModifs & CTRL) != 0 && (nModifs & ~CTRL) == 0)
+	    else if ((nModifs & CTRL) && !(nModifs & ~CTRL))
 		*pch = VirtKeyMap[i].chCtrl;
-	    else if ((nModifs & ALT) != 0 && (nModifs & ~ALT) == 0)
+	    else if ((nModifs & ALT) && (vtp_working || !(nModifs & ~ALT)))
 		*pch = VirtKeyMap[i].chAlt;
 
 	    if (*pch != 0)
@@ -1133,6 +1133,70 @@ decode_key_event(
 		{
 		    *pch2 = *pch;
 		    *pch = K_NUL;
+		    if (pmodifiers && vtp_working)
+		    {
+			if (pker->wVirtualKeyCode >= VK_F1
+			    && pker->wVirtualKeyCode <= VK_F12)
+			{
+			    if (nModifs & ALT)
+			    {
+				*pmodifiers |= MOD_MASK_ALT;
+				if (!(nModifs & SHIFT))
+				    *pch2 = VirtKeyMap[i].chAlone;
+			    }
+			    if ((nModifs & CTRL))
+			    {
+				*pmodifiers |= MOD_MASK_CTRL;
+				if (!(nModifs & SHIFT))
+				    *pch2 = VirtKeyMap[i].chAlone;
+			    }
+			}
+			else if (pker->wVirtualKeyCode >= VK_END
+				&& pker->wVirtualKeyCode <= VK_DOWN)
+			{
+			    // VK_END   0x23
+			    // VK_HOME  0x24
+			    // VK_LEFT  0x25
+			    // VK_UP    0x26
+			    // VK_RIGHT 0x27
+			    // VK_DOWN  0x28
+			    *pmodifiers = 0;
+			    *pch2 = VirtKeyMap[i].chAlone;
+			    if ((nModifs & SHIFT) && !(nModifs & ~SHIFT))
+			    {
+				*pch2 = VirtKeyMap[i].chShift;
+			    }
+			    else if ((nModifs & CTRL) && !(nModifs & ~CTRL))
+			    {
+				*pch2 = VirtKeyMap[i].chCtrl;
+				if (pker->wVirtualKeyCode == VK_UP
+				    || pker->wVirtualKeyCode == VK_DOWN)
+				{
+				    *pmodifiers |= MOD_MASK_CTRL;
+				    *pch2 = VirtKeyMap[i].chAlone;
+				}
+			    }
+			    else if ((nModifs & ALT) && !(nModifs & ~ALT))
+			    {
+				*pch2 = VirtKeyMap[i].chAlt;
+			    }
+			    else if ((nModifs & SHIFT) && (nModifs & CTRL))
+			    {
+				*pmodifiers |= MOD_MASK_CTRL;
+				*pch2 = VirtKeyMap[i].chShift;
+			    }
+			}
+			else
+			{
+			    *pch2 = VirtKeyMap[i].chAlone;
+			    if (nModifs & SHIFT)
+				*pmodifiers |= MOD_MASK_SHIFT;
+			    if (nModifs & CTRL)
+				*pmodifiers |= MOD_MASK_CTRL;
+			    if (nModifs & ALT)
+				*pmodifiers |= MOD_MASK_ALT;
+			}
+		    }
 		}
 
 		return TRUE;
@@ -1152,18 +1216,18 @@ decode_key_event(
 	{
 	    // Pass on the ALT key as a modifier, but only when not combined
 	    // with CTRL (which is ALTGR).
-	    if ((nModifs & ALT) != 0 && (nModifs & CTRL) == 0)
+	    if ((nModifs & ALT) && !(nModifs & CTRL))
 		*pmodifiers |= MOD_MASK_ALT;
 
 	    // Pass on SHIFT only for special keys, because we don't know when
 	    // it's already included with the character.
-	    if ((nModifs & SHIFT) != 0 && *pch <= 0x20)
+	    if ((nModifs & SHIFT) && *pch <= 0x20)
 		*pmodifiers |= MOD_MASK_SHIFT;
 
 	    // Pass on CTRL only for non-special keys, because we don't know
 	    // when it's already included with the character.  And not when
 	    // combined with ALT (which is ALTGR).
-	    if ((nModifs & CTRL) != 0 && (nModifs & ALT) == 0
+	    if ((nModifs & CTRL) && !(nModifs & ALT)
 					       && *pch >= 0x20 && *pch < 0x80)
 		*pmodifiers |= MOD_MASK_CTRL;
 	}
@@ -2413,6 +2477,8 @@ mch_inchar(
 	    int		modifiers = 0;
 
 	    c = tgetch(&modifiers, &ch2);
+
+	    c = simplify_key(c, &modifiers);
 
 	    // Some chars need adjustment when the Ctrl modifier is used.
 	    ++no_reduce_keys;
