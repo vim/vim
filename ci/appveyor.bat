ad15a39fdbde5ef8d4af9e0fca7e7e53b4843270
Christopher Plewright <chris@createng.com>
2023-01-11 12:49:22 +0000

    patch 9.0.1177: AppVeyor uses some older tools
    Problem:    AppVeyor uses some older tools.
    Solution:   Switch to Visual Studio 2022 and Python 3.11. (Christopher
                Plewright, closes #11793)

--- ci/appveyor.bat
+++ ci/appveyor.bat
@@ -22,7 +22,7 @@ if "%FEATURE%" == "HUGE" (
     nmake -f Make_mvc.mak CPU=AMD64 ^
         OLE=no GUI=yes IME=yes ICONV=yes DEBUG=no POSTSCRIPT=yes ^
         PYTHON_VER=27 DYNAMIC_PYTHON=yes PYTHON=C:\Python27-x64 ^
-        PYTHON3_VER=35 DYNAMIC_PYTHON3=yes PYTHON3=C:\Python35-x64 ^
+        PYTHON3_VER=311 DYNAMIC_PYTHON3=yes PYTHON3=C:\Python311-x64 ^
         FEATURES=%FEATURE%
 ) ELSE (
     nmake -f Make_mvc.mak CPU=AMD64 ^
