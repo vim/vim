3c204eee00b9f9acdb864977a0fec60f91f065ec
Christopher Plewright <chris@createng.com>
2023-01-15 10:00:50 +1100

    separate vim and gvim build

--- ci/appveyor.bat
+++ ci/appveyor.bat
@@ -1,15 +1,14 @@
 @echo off
 :: Batch file for building/testing Vim on AppVeyor
 
+setlocal ENABLEDELAYEDEXPANSION
+cd %APPVEYOR_BUILD_FOLDER%
+
 :: Python3
 set PYTHON3_VER=311
 set PYTHON3_RELEASE=3.11.1
 set PYTHON3_URL=https://www.python.org/ftp/python/%PYTHON3_RELEASE%/python-%PYTHON3_RELEASE%-amd64.exe
 set PYTHON3_DIR=C:\python%PYTHON3_VER%-x64
-
-
-setlocal ENABLEDELAYEDEXPANSION
-cd %APPVEYOR_BUILD_FOLDER%
 
 if not exist downloads mkdir downloads
 
@@ -21,21 +20,31 @@ if not exist %PYTHON3_DIR% (
 
 cd src
 
-:: build MSVC huge version with python and channel support
-echo "Building MSVC 64bit GUI and console Version"
-
+echo "Building MSVC 64bit console Version"
 nmake -f Make_mvc.mak CPU=AMD64 ^
-    OLE=no GUI=yes VIMDLL=yes TERMINAL=yes IME=yes ICONV=yes DEBUG=no POSTSCRIPT=yes ^
-    PYTHON_VER=27 DYNAMIC_PYTHON=yes PYTHON=C:\Python27-x64 ^
-    PYTHON3_VER=%PYTHON3_VER% DYNAMIC_PYTHON3=yes PYTHON3=%PYTHON3_DIR% ^
-    FEATURES=HUGE
-
-if not exist gvim.exe (
-    echo Build failure: no gvim.exe
+    OLE=no GUI=no IME=yes ICONV=yes DEBUG=no ^
+    FEATURES=%FEATURE%
+if not exist vim.exe (
+    echo Build failure.
     exit 1
 )
-if not exist vim.exe (
-    echo Build failure: no vim.exe
+
+:: build MSVC huge version with python and channel support
+:: GUI needs to be last, so that testing works
+echo "Building MSVC 64bit GUI Version"
+if "%FEATURE%" == "HUGE" (
+    nmake -f Make_mvc.mak CPU=AMD64 ^
+        OLE=no GUI=yes IME=yes ICONV=yes DEBUG=no POSTSCRIPT=yes ^
+        PYTHON_VER=27 DYNAMIC_PYTHON=yes PYTHON=C:\Python27-x64 ^
+        PYTHON3_VER=%PYTHON3_VER% DYNAMIC_PYTHON3=yes PYTHON3=%PYTHON3_DIR% ^
+        FEATURES=%FEATURE%
+) ELSE (
+    nmake -f Make_mvc.mak CPU=AMD64 ^
+        OLE=no GUI=yes IME=yes ICONV=yes DEBUG=no ^
+        FEATURES=%FEATURE%
+)
+if not exist gvim.exe (
+    echo Build failure.
     exit 1
 )
 .\gvim -u NONE -c "redir @a | ver |0put a | wq" ver_msvc.txt || exit 1
@@ -47,9 +56,9 @@ type ver_msvc.txt || exit 1
 cd ..
 
 goto :eof
+:: ----------------------------------------------------------------------
 
 :downloadfile
-:: ----------------------------------------------------------------------
 :: call :downloadfile <URL> <localfile>
 if not exist %2 (
 	curl -f -L %1 -o %2
