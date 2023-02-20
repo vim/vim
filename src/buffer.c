e1121b139480f53d1b06f84f3e4574048108fa0b
Pavel Mayorov <pmayorov@cloudlinux.com>
2023-02-20 14:35:20 +0000

    patch 9.0.1331: illegal memory access when using :ball in Visual mode
    Problem:    Illegal memory access when using :ball in Visual mode.
    Solution:   Stop Visual mode when using :ball. (Pavel Mayorov, closes #11923)

--- src/buffer.c
+++ src/buffer.c
@@ -5402,6 +5402,10 @@ ex_buffer_all(exarg_T *eap)
     else
 	all = TRUE;
 
+    // Stop Visual mode, the cursor and "VIsual" may very well be invalid after
+    // switching to another buffer.
+    reset_VIsual_and_resel();
+
     setpcmark();
 
 #ifdef FEAT_GUI
