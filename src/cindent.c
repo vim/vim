8c24637d49175ffef05a7ea85249af3b50a47597
Virginia Senioria <91khr@users.noreply.github.com>
2023-03-11 16:00:32 +0800

    fix cindent: fix for `export namespace` in C++20

--- src/cindent.c
+++ src/cindent.c
@@ -769,7 +769,7 @@ cin_is_cpp_namespace(char_u *s)
 
     s = cin_skipcomment(s);
 
-    if (STRNCMP(s, "inline", 6) == 0 && (s[6] == NUL || !vim_iswordc(s[6])))
+    while ((STRNCMP(s, "inline", 6) == 0 || STRNCMP(s, "export", 6) == 0) && (s[6] == NUL || !vim_iswordc(s[6])))
 	s = cin_skipcomment(skipwhite(s + 6));
 
     if (STRNCMP(s, "namespace", 9) == 0 && (s[9] == NUL || !vim_iswordc(s[9])))
