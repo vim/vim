99e4ab2a1e577ddb29030c09c308b67e16fd51c4
Virginia Senioria <91khr@users.noreply.github.com>
2023-03-24 19:25:06 +0000

    patch 9.0.1426: indent wrong after "export namespace" in C++
    Problem:    Indent wrong after "export namespace" in C++.
    Solution:   Skip over "inline" and "export" in any order. (Virginia Senioria,
                closes #12134, closes #12133)

--- src/cindent.c
+++ src/cindent.c
@@ -769,7 +769,9 @@ cin_is_cpp_namespace(char_u *s)
 
     s = cin_skipcomment(s);
 
-    if (STRNCMP(s, "inline", 6) == 0 && (s[6] == NUL || !vim_iswordc(s[6])))
+    // skip over "inline" and "export" in any order
+    while ((STRNCMP(s, "inline", 6) == 0 || STRNCMP(s, "export", 6) == 0)
+					&& (s[6] == NUL || !vim_iswordc(s[6])))
 	s = cin_skipcomment(skipwhite(s + 6));
 
     if (STRNCMP(s, "namespace", 9) == 0 && (s[9] == NUL || !vim_iswordc(s[9])))
