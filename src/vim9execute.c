d4a9b7f61475f3166cce92b0bad748e8c1eb51fe
Bram Moolenaar <Bram@vim.org>
2023-05-23 14:48:42 +0100

    patch 9.0.1573: error for function name has wrong line number
    Problem:    Error for function name has wrong line number.
    Solution:   Set the line number before giving the error.

--- src/vim9execute.c
+++ src/vim9execute.c
@@ -4448,6 +4448,7 @@ exec_instructions(ectx_T *ectx)
 		    CLEAR_FIELD(ea);
 		    ea.cmd = ea.arg = iptr->isn_arg.string;
 		    ga_init2(&lines_to_free, sizeof(char_u *), 50);
+		    SOURCING_LNUM = iptr->isn_lnum;
 		    define_function(&ea, NULL, &lines_to_free, 0);
 		    ga_clear_strings(&lines_to_free);
 		}
