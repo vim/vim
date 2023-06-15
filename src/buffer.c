2f13a3c38bba42b869de4757eddd46d1dfb86761
zeertzjq <zeertzjq@outlook.com>
2023-06-15 17:37:40 +0800

    Fix typo

--- src/buffer.c
+++ src/buffer.c
@@ -502,7 +502,7 @@ can_unload_buffer(buf_T *buf)
  * It can be:
  * 0			buffer becomes hidden
  * DOBUF_UNLOAD		buffer is unloaded
- * DOBUF_DELETE		buffer is unloaded and removed from buffer list
+ * DOBUF_DEL		buffer is unloaded and removed from buffer list
  * DOBUF_WIPE		buffer is unloaded and really deleted
  * DOBUF_WIPE_REUSE	idem, and add to buf_reuse list
  * When doing all but the first one on the current buffer, the caller should
