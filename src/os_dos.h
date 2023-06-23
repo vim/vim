801961d2c2876e5d2d0b52a585b9d04535168ebd
Bram Moolenaar <Bram@vim.org>
2023-06-23 16:15:13 +0100

    patch 9.0.1650: MS-Windows: default 'viewdir' may include read-only directory
    Problem:    MS-Windows: default 'viewdir' may include read-only directory.
    Solution:   Use $HOME instead of $VIM for 'viewdir' default. (closes #12119)

--- src/os_dos.h
+++ src/os_dos.h
@@ -97,7 +97,7 @@
 #endif
 
 #ifndef DFLT_VDIR
-# define DFLT_VDIR	"$VIM/vimfiles/view"	// default for 'viewdir'
+# define DFLT_VDIR	"$HOME/vimfiles/view"	// default for 'viewdir'
 #endif
 
 #ifndef DFLT_DIR
@@ -105,7 +105,7 @@
 #endif
 
 #define DFLT_ERRORFILE		"errors.err"
-#define DFLT_RUNTIMEPATH	"$HOME/vimfiles,$VIM/vimfiles,$VIMRUNTIME,$VIM/vimfiles/after,$HOME/vimfiles/after"
+#define DFLT_RUNTIMEPATH	"$HOME/vimfiles,$VIM/vimfiles,$VIMRUNTIME,$HOME/vimfiles/after,$VIM/vimfiles/after"
 #define CLEAN_RUNTIMEPATH	"$VIM/vimfiles,$VIMRUNTIME,$VIM/vimfiles/after"
 
 #define CASE_INSENSITIVE_FILENAME   // ignore case when comparing file names
