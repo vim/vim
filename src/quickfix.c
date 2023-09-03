fc68299d436cf87453e432daa77b6d545df4d7ed
Christian Brabandt <cb@256bit.org>
2023-09-03 20:20:52 +0200

    patch 9.0.1857: [security] heap-use-after-free in is_qf_win()
    Problem:  heap-use-after-free in is_qf_win()
    Solution: Check buffer is valid before accessing it
    
    Signed-off-by: Christian Brabandt <cb@256bit.org>

--- src/quickfix.c
+++ src/quickfix.c
@@ -4492,7 +4492,7 @@ is_qf_win(win_T *win, qf_info_T *qi)
     // set to NULL.
     // A window displaying a location list buffer will have the w_llist_ref
     // pointing to the location list.
-    if (bt_quickfix(win->w_buffer))
+    if (buf_valid(win->w_buffer) && bt_quickfix(win->w_buffer))
 	if ((IS_QF_STACK(qi) && win->w_llist_ref == NULL)
 		|| (IS_LL_STACK(qi) && win->w_llist_ref == qi))
 	    return TRUE;
