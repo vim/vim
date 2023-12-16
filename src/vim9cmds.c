4fb779f5be26ae302e109f87748a119233722de6
Yegappan Lakshmanan <yegappan@yahoo.com>
2023-12-15 19:45:08 -0800

    Vim crashes when compiling a for statement with a non-existing type

--- src/vim9cmds.c
+++ src/vim9cmds.c
@@ -1088,6 +1088,8 @@ compile_for(char_u *arg_start, cctx_T *cctx)
 		}
 		p = skipwhite(p + 1);
 		lhs_type = parse_type(&p, cctx->ctx_type_list, TRUE);
+		if (lhs_type == NULL)
+		    goto failed;
 	    }
 
 	    if (get_var_dest(name, &dest, CMD_for, &opt_flags,
