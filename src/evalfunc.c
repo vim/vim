b1cdb53cdd5b40a7fd9015f28de7487c7abd6e86
Shougo Matsushita <Shougo.Matsu@gmail.com>
2024-02-25 16:33:32 +0900

    Fix diff

--- src/evalfunc.c
+++ src/evalfunc.c
@@ -5513,12 +5513,12 @@ f_getregion(typval_T *argvars, typval_T *rettv)
 
     // NOTE: var2fpos() returns static pointer.
     fp = var2fpos(&argvars[0], TRUE, &fnum, FALSE);
-    if (fp == NULL || fnum >= 0 && fnum != curbuf->b_fnum)
+    if (fp == NULL || (fnum >= 0 && fnum != curbuf->b_fnum))
 	return;
     p1 = *fp;
 
     fp = var2fpos(&argvars[1], TRUE, &fnum, FALSE);
-    if (fp == NULL || fnum >= 0 && fnum != curbuf->b_fnum)
+    if (fp == NULL || (fnum >= 0 && fnum != curbuf->b_fnum))
 	return;
     p2 = *fp;
 
