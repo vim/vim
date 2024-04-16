545ef516d85b3f1d343c17e1ef4d57c7bf12601c
Diego Viola <diego.viola@gmail.com>
2024-04-16 11:59:20 -0300

    Missing XDG vimrc entry in :version
    Problem:  Missing XDG vimrc entry in :version
    Solution: Add XDG vimrc file to :version (Christian Brabandt)
    
    related: #14182
    
    Signed-off-by: Diego Viola <diego.viola@gmail.com>

--- src/version.c
+++ src/version.c
@@ -1743,9 +1743,20 @@ list_version(void)
     version_msg(USR_VIMRC_FILE2);
     version_msg("\"\n");
 #endif
-#ifdef USR_VIMRC_FILE3
+#if defined(USR_VIMRC_FILE3) && defined(XDG_VIMRC_FILE)
+# ifdef USR_VIMRC_FILE3
     version_msg(_(" 3rd user vimrc file: \""));
     version_msg(USR_VIMRC_FILE3);
+    version_msg("\"\n");
+# endif
+# ifdef XDG_VIMRC_FILE
+    version_msg(_(" 4th user vimrc file: \""));
+    version_msg((char *)(XDG_VIMRC_FILE));
+    version_msg("\"\n");
+# endif
+#elif defined(XDG_VIMRC_FILE)
+    version_msg(_(" 3rd user vimrc file: \""));
+    version_msg((char *)(XDG_VIMRC_FILE));
     version_msg("\"\n");
 #endif
 #ifdef USR_EXRC_FILE
