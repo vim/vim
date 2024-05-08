88c34575227fc32b8d09917b2deaea1af90c744b
zeertzjq <zeertzjq@outlook.com>
2024-05-08 10:40:11 +0800

    Wrong display with 'smoothscroll' when changing quickfix list
    Problem:  Wrong display with 'smoothscroll' when changing quickfix list.
    Solution: Reset w_skipcol when replacing quickfix list.

--- src/quickfix.c
+++ src/quickfix.c
@@ -4867,6 +4867,9 @@ qf_fill_buffer(qf_list_T *qfl, buf_T *buf, qfline_T *old_last, int qf_winid)
 
     if (old_last == NULL)
     {
+	win_T		*wp;
+	tabpage_T	*tp;
+
 	if (buf != curbuf)
 	{
 	    internal_error("qf_fill_buffer()");
@@ -4882,6 +4885,10 @@ qf_fill_buffer(qf_list_T *qfl, buf_T *buf, qfline_T *old_last, int qf_winid)
 	// otherwise autocommands may invalidate the undo stack
 	while ((curbuf->b_ml.ml_flags & ML_EMPTY) == 0)
 	    (void)ml_delete((linenr_T)1);
+
+	FOR_ALL_TAB_WINDOWS(tp, wp)
+	    if (wp->w_buffer == curbuf)
+		wp->w_skipcol = 0;
 
 	// Remove all undo information
 	u_clearallandblockfree(curbuf);
