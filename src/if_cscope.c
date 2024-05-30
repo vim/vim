51024bbc1a9e298b1fb8f2e465fccb5db409551e
Mike Williams <mrmrdubya@gmail.com>
2024-05-30 07:46:30 +0200

    patch 9.1.0449: MS-Windows: Compiler warnings
    Problem:  MS-Windows: Compiler warnings
    Solution: Resolve size_t to int warnings
    
    closes: #14874
    
    A couple of warnings in ex_docmd.c have been resolved by modifying their
    function argument types, followed by some changes in various function
    call sites.  This also allowed removal of some casts to cope with
    size_t/int conversion.
    
    Signed-off-by: Mike Williams <mrmrdubya@gmail.com>
    Signed-off-by: Christian Brabandt <cb@256bit.org>

--- src/if_cscope.c
+++ src/if_cscope.c
@@ -541,7 +541,7 @@ cs_add_common(
     char	*ppath = NULL;
     int		i;
     int		len;
-    int		usedlen = 0;
+    size_t	usedlen = 0;
     char_u	*fbuf = NULL;
 
     // get the filename (arg1), expand it, and try to stat it
