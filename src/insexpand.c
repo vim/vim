84fbfb69ce1f827e77c54ba3511ac65a7e2394d1
glepnir <glephunter@gmail.com>
2024-07-28 20:29:15 +0800

    feat(completion): support insert special characters when completion
    Problem: Lsp completion result have special characters liek \n \t in snippet.
    Currently completion behavior just insert them as blob.
    
    Solution: handle these characters specially in ins_compl_insert.

--- src/insexpand.c
+++ src/insexpand.c
@@ -179,6 +179,7 @@ static pos_T	  compl_startpos;
 // Length in bytes of the text being completed (this is deleted to be replaced
 // by the match.)
 static int	  compl_length = 0;
+static linenr_T	  compl_lnum = 0;           // lnum where the completion start
 static colnr_T	  compl_col = 0;	    // column where the text starts
 					    // that is being completed
 static char_u	  *compl_orig_text = NULL;  // text as it was before
@@ -4138,11 +4139,21 @@ ins_compl_delete(void)
     // In insert mode: Delete the typed part.
     // In replace mode: Put the old characters back, if any.
     col = compl_col + (compl_status_adding() ? compl_length : 0);
-    if ((int)curwin->w_cursor.col > col)
-    {
-	if (stop_arrow() == FAIL)
-	    return;
-	backspace_until_column(col);
+    while (curwin->w_cursor.lnum > compl_lnum
+	    || (curwin->w_cursor.lnum == compl_lnum
+		&& (int)curwin->w_cursor.col > col)) {
+      if (curwin->w_cursor.lnum > compl_lnum)
+      {
+	  ml_delete(curwin->w_cursor.lnum);
+	  curwin->w_cursor.lnum--;
+	  curwin->w_cursor.col = (colnr_T)STRLEN(ml_get(curwin->w_cursor.lnum));
+      }
+      else
+      {
+	  if (stop_arrow() == FAIL)
+	      return;
+	  backspace_until_column(col);
+      }
     }
 
     // TODO: is this sufficient for redrawing?  Redrawing everything causes
@@ -4152,6 +4163,26 @@ ins_compl_delete(void)
     // clear v:completed_item
     set_vim_var_dict(VV_COMPLETED_ITEM, dict_alloc_lock(VAR_FIXED));
 #endif
+}
+
+/* 
+ * handle sepecial characters in word like wrap tab etc.
+ */
+    static void
+handle_special_chars(char_u *str)
+{
+    while (*str) {
+	if (*str == '\n')
+	{
+	  open_line(FORWARD, OPENLINE_DELSPACES, FALSE, NULL);
+	  curwin->w_cursor.col = 0;
+	}
+	else if (*str == '\t')
+	    ins_char('\t');
+	else
+	    ins_char(*str);
+	str++;
+    }
 }
 
 /*
@@ -4166,7 +4197,7 @@ ins_compl_insert(int in_compl_func)
     // Make sure we don't go over the end of the string, this can happen with
     // illegal bytes.
     if (compl_len < (int)STRLEN(compl_shown_match->cp_str))
-	ins_bytes(compl_shown_match->cp_str + compl_len);
+        handle_special_chars(compl_shown_match->cp_str + compl_len);
     if (match_at_original_text(compl_shown_match))
 	compl_used_match = FALSE;
     else
@@ -5123,6 +5154,7 @@ ins_compl_start(void)
     line = ml_get(curwin->w_cursor.lnum);
     curs_col = curwin->w_cursor.col;
     compl_pending = 0;
+    compl_lnum = curwin->w_cursor.lnum;
 
     if ((compl_cont_status & CONT_INTRPT) == CONT_INTRPT
 	    && compl_cont_mode == ctrl_x_mode)
