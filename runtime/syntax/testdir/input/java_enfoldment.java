05d0893ed8d991ed3fa23da7c7d4efe6ffd58798
Aliaksei Budavei <0x000c70@gmail.com>
2024-10-12 17:12:51 +0200

    runtime(java): Define javaBlockStart and javaBlockOtherStart hl groups
    And do not link either to any group.
    
    Resolves zzzyxwvut/java-vim#2.
    
    Signed-off-by: Aliaksei Budavei <0x000c70@gmail.com>
    Signed-off-by: Christian Brabandt <cb@256bit.org>

--- runtime/syntax/testdir/input/java_enfoldment.java
+++ runtime/syntax/testdir/input/java_enfoldment.java
@@ -1,5 +1,8 @@
 // VIM_TEST_SETUP setlocal foldenable foldcolumn=2 foldmethod=syntax
 // VIM_TEST_SETUP let g:java_foldtext_show_first_or_second_line = 1
+// VIM_TEST_SETUP highlight link javaBlockOtherStart Structure
+// VIM_TEST_SETUP highlight link javaBlockStart Todo
+
 
 	@SuppressWarnings({
 	"""
@@ -10,6 +13,9 @@
 	/**
 	 *
 	 */
+	///
+	///
+	///
 	//
 	//
 	//
@@ -78,6 +84,21 @@ out: {
 	/** No operation. */
 	void noOp4() {
 	/*/\/\/\*/ ; }
+
+	/// No operation.
+	///
+	///
+	void noOp5() { }
+	/// No operation.
+	void noOp6()
+	{
+	}
+	/// No operation.
+	void noOp7() {
+	}
+	/// No operation.
+	void noOp8() {
+	/*/\/\/\*/ ; }
 }
 
 /*
@@ -90,6 +111,9 @@ out: {
  * {
  * }
  */
+/// A summary.
+/// {
+/// }
 //
 // {
 // }
