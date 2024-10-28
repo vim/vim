f32c1a5add6d26b1689a3b3256b97802d19b117a
basilisk0315 <basilisk@internode.on.net>
2024-10-28 12:14:17 +1100

    Fix build failure

--- src/register.c
+++ src/register.c
@@ -2557,7 +2557,7 @@ dnd_yank_drag_data(char_u *str, long len)
     yankreg_T *curr;
 
     curr = y_current;
-    y_current = &y_regs[TILDE_REGISTER].string;
+    y_current = &y_regs[TILDE_REGISTER];
     free_yank_all();
     str_to_reg(y_current, MCHAR, str, len, 0L, FALSE);
     y_current = curr;
