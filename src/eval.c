9dd69672173a76c7f224527ec73a8846f952fedf
basilisk0315 <basilisk@internode.on.net>
2024-11-16 13:27:38 +1100

    Correct retval in make_expanded_name()

--- src/eval.c
+++ src/eval.c
@@ -6924,12 +6924,15 @@ make_expanded_name(
     temp_result = eval_to_string(expr_start + 1, FALSE, FALSE);
     if (temp_result != NULL)
     {
-	size_t	retvalsize = (expr_start - in_start) + STRLEN(temp_result)
+	size_t	in_startlen = (size_t)(expr_start - in_start);
+	size_t	retvalsize = in_startlen + STRLEN(temp_result)
 						+ (in_end - expr_end) + 1;
 
 	retval = alloc(retvalsize);
 	if (retval != NULL)
-	    vim_snprintf((char *)retval, retvalsize, "%s%s%s", in_start, temp_result, expr_end + 1);
+	    vim_snprintf((char *)retval, retvalsize, "%*.*s%s%s",
+				in_startlen, in_startlen,
+				in_start, temp_result, expr_end + 1);
     }
     vim_free(temp_result);
 
