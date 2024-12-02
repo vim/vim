92f4538c5bb82574cd264cf35f33e80a2098930f
Jonathon <jonathonwhite@protonmail.com>
2024-12-02 11:07:55 -0500

    add modeline

--- src/linematch.c
+++ src/linematch.c
@@ -1,4 +1,12 @@
-// vi:set ts=8 sts=4 sw=4 noet:
+/* vi:set ts=8 sts=4 sw=4 noet:
+ *
+ * VIM - Vi IMproved	by Bram Moolenaar
+ *
+ * Do ":help uganda"  in Vim to read copying and usage conditions.
+ * Do ":help credits" in Vim to see a list of people who contributed.
+ * See README.txt for an overview of the Vim source code.
+ */
+
 #include <assert.h>
 #include <math.h>
 #include <stdbool.h>
