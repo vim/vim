6a2adb015a9ff0b969ed323671d8273c511d82d8
Enan Ajmain <3nan.ajmain@gmail.com>
2024-12-30 10:41:02 -0500

    Fix indentation

--- src/gui.c
+++ src/gui.c
@@ -1429,7 +1429,7 @@ gui_position_components(int total_width UNUSED)
     text_area_x = 0;
     if (gui.which_scrollbars[SBAR_LEFT])
 	text_area_x += gui.scrollbar_width;
-	text_area_x += p_guipad;
+    text_area_x += p_guipad;
 
     text_area_y = 0;
 #if defined(FEAT_MENU) && !(defined(FEAT_GUI_GTK) || defined(FEAT_GUI_PHOTON))
