4395dbeb3ad448e16d28157eb53a32d3b0c3cd84
Yegappan Lakshmanan <yegappan@yahoo.com>
2025-01-02 22:01:37 -0800

    Give an error if an unintialized object variable is referenced in new()

--- src/errors.h
+++ src/errors.h
@@ -3616,8 +3616,10 @@ EXTERN char e_duplicate_enum_str[]
 	INIT(= N_("E1428: Duplicate enum value: %s"));
 EXTERN char e_class_can_only_be_used_in_script[]
 	INIT(= N_("E1429: Class can only be used in a script"));
+EXTERN char e_uninitialized_object_var_reference[]
+	INIT(= N_("E1430: Uninitialized object variable '%s' referenced"));
 #endif
-// E1429 - E1499 unused (reserved for Vim9 class support)
+// E1431 - E1499 unused (reserved for Vim9 class support)
 EXTERN char e_cannot_mix_positional_and_non_positional_str[]
 	INIT(= N_("E1500: Cannot mix positional and non-positional arguments: %s"));
 EXTERN char e_fmt_arg_nr_unused_str[]
