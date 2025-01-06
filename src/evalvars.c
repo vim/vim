6655bef33047b826e0ccb8c686f3f57e47161b1c
zeertzjq <zeertzjq@outlook.com>
2025-01-06 18:32:13 +0100

    patch 9.1.0991: v:stacktrace has wrong type in Vim9 script
    Problem:  v:stacktrace has wrong type in Vim9 script.
    Solution: Change the type to t_list_dict_any.  Fix grammar in docs.
              (zeertzjq)
    
    closes: #16390
    
    Signed-off-by: zeertzjq <zeertzjq@outlook.com>
    Signed-off-by: Christian Brabandt <cb@256bit.org>

--- src/evalvars.c
+++ src/evalvars.c
@@ -161,7 +161,7 @@ static struct vimvar
     {VV_NAME("t_typealias",	 VAR_NUMBER), NULL, VV_RO},
     {VV_NAME("t_enum",		 VAR_NUMBER), NULL, VV_RO},
     {VV_NAME("t_enumvalue",	 VAR_NUMBER), NULL, VV_RO},
-    {VV_NAME("stacktrace",	 VAR_LIST), &t_list_string, VV_RO},
+    {VV_NAME("stacktrace",	 VAR_LIST), &t_list_dict_any, VV_RO},
 };
 
 // shorthand
