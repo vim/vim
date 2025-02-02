001c26cd6194fba2bfccb06dec30fdc9e1410e62
glepnir <glephunter@gmail.com>
2025-02-02 09:36:22 +0100

    patch 9.1.1069: preinsert text completions not deleted with <C-W>/<C-U>
    Problem:  preinsert text completions not deleted with <C-W>/<C-U>
              (ddad431, after v9.1.1059)
    Solution: handle <C-W> or <C-U> specifically and clear the completion
              (glepnir)
    
    fixes: #16557
    closes: #16565
    
    Signed-off-by: glepnir <glephunter@gmail.com>
    Signed-off-by: Christian Brabandt <cb@256bit.org>

--- src/insexpand.c
+++ src/insexpand.c
@@ -2498,6 +2498,9 @@ ins_compl_stop(int c, int prev_mode, int retval)
 	retval = TRUE;
     }
 
+    if ((c == Ctrl_W || c == Ctrl_U) && ins_compl_preinsert_effect())
+	ins_compl_delete();
+
     auto_format(FALSE, TRUE);
 
     // Trigger the CompleteDonePre event to give scripts a chance to
