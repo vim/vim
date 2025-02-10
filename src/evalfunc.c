839019d68c0c7fc75ad9d02b64cbe03d03745556
S0AndS0 <strangerthanbland@gmail.com>
2025-02-09 17:44:02 -0800

    Check if target buffer is active buffer...
    - If so then use code that works!
    - Else use code that not work :-(
    
    Though this may feel like shifting veggies around instead of eating 'em,
    at least previously functional behavior is preserved when operating on
    an active buffer.  And may be slightly more efficient once things do
    work as we avoid assigning variables until necessary.

--- src/evalfunc.c
+++ src/evalfunc.c
@@ -10944,21 +10944,7 @@ set_position(typval_T *argvars, typval_T *rettv, int charpos)
 	pos.col = 0;
     if ((name[0] == '.' && name[1] == NUL))
     {
-	/**
-	 * fnum is the buffer number, but needs to be converted to `typval_T
-	 * *tv` for functions like `tv_get_buf` to be useful for obtaining the
-	 * buffer data, from there we got to check if returned buffer is `NULL`
-	 * and if so do the default thang, and if not do some bounds checking
-	 * prior to mutating buffer data..
-	 */
-	typval_T    tv;
-	tv.v_type = VAR_NUMBER;
-	tv.vval.v_number = fnum;
-
-	buf_T      *buf;
-	buf = tv_get_buf(&tv, TRUE);
-
-	if (buf == NULL) {
+	if (fnum == curwin->w_buffer->b_fnum) {
 	    // set cursor; "fnum" is ignored
 	    curwin->w_cursor = pos;
 	    if (curswant >= 0)
@@ -10968,14 +10954,33 @@ set_position(typval_T *argvars, typval_T *rettv, int charpos)
 	    }
 	    check_cursor();
 	    rettv->vval.v_number = 0;
-	} else if (
-		   pos.lnum > 1 && pos.lnum <= buf->b_ml.ml_line_count
-		&& pos.col  > 0 && pos.col  <= ml_get_buf_len(buf, pos.lnum)
-	) {
-	    buf->b_last_cursor = pos;
-	    rettv->vval.v_number = 0;
 	} else {
-	    emsg("Error: setpos line row or column out of bounds for target buffer");
+	    /**
+	     * fnum is the buffer number, but needs to be converted to `typval_T
+	     * *tv` for functions like `tv_get_buf` to be useful for obtaining the
+	     * buffer data, from there we got to check if returned buffer is `NULL`
+	     * and if so do the default thang, and if not do some bounds checking
+	     * prior to mutating buffer data..
+	     */
+	    typval_T    tv;
+	    tv.v_type = VAR_NUMBER;
+	    tv.vval.v_number = fnum;
+
+	    buf_T      *buf;
+	    buf = tv_get_buf(&tv, TRUE);
+
+	    /* printf("fnum -> %i | pos.lnum -> %i | buf->b_ml.ml_line_count -> %i | pos.col -> %i | ml_get_buf_len(buf, pos.lnum) -> %i | curwin->w_buffer->b_fnum %i\n", */
+	    /* 	fnum,        pos.lnum,        buf->b_ml.ml_line_count,        pos.col,        ml_get_buf_len(buf, pos.lnum),        curwin->w_buffer->b_fnum ); */
+
+	    if (
+		       pos.lnum > 1 && pos.lnum <= buf->b_ml.ml_line_count
+		    && pos.col >= 0 && pos.col  <= ml_get_buf_len(buf, pos.lnum)
+	    ) {
+		buf->b_last_cursor = pos;
+		rettv->vval.v_number = 0;
+	    } else {
+		emsg("Error: setpos line row or column out of bounds for target buffer");
+	    }
 	}
     }
     else if (name[0] == '\'' && name[1] != NUL && name[2] == NUL)
