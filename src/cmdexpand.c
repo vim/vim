e81b7ec9ea5c00851ce2807915ce8a05e9956a81
Jim Zhou <csd_189@163.com>
2025-02-26 13:27:55 +0800

    Compatibility fix for tiny build

--- src/cmdexpand.c
+++ src/cmdexpand.c
@@ -229,12 +229,14 @@ nextwild(
 
     if (xp->xp_numfiles == -1)
     {
+#ifdef FEAT_EVAL
     if (ccline->input_fn && ccline->xp_context == EXPAND_COMMANDS)
     {
     // Expand commands in input() function
     set_cmd_context(xp, ccline->cmdbuff, ccline->cmdlen, ccline->cmdpos, FALSE);
     }
     else
+#endif
     {
     set_expand_context(xp);
     cmd_showtail = expand_showtail(xp);
