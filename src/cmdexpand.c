c6e43a64868181c933539cbae496806cf6b53a17
Jim Zhou <jimzhouzzy@gmail.com>
2025-02-27 08:10:50 +0800

    fix indentation attempt

--- src/cmdexpand.c
+++ src/cmdexpand.c
@@ -230,17 +230,17 @@ nextwild(
     if (xp->xp_numfiles == -1)
     {
 #ifdef FEAT_EVAL
-    if (ccline->input_fn && ccline->xp_context == EXPAND_COMMANDS)
-    {
-    // Expand commands typed in input() function
-    set_cmd_context(xp, ccline->cmdbuff, ccline->cmdlen, ccline->cmdpos, FALSE);
-    }
-    else
+        if (ccline->input_fn && ccline->xp_context == EXPAND_COMMANDS)
+        {
+        // Expand commands typed in input() function
+        set_cmd_context(xp, ccline->cmdbuff, ccline->cmdlen, ccline->cmdpos, FALSE);
+        }
+        else
 #endif
-    {
-    set_expand_context(xp);
-    }
-    cmd_showtail = expand_showtail(xp);
+        {
+        set_expand_context(xp);
+        }
+        cmd_showtail = expand_showtail(xp);
     }
 
     if (xp->xp_context == EXPAND_UNSUCCESSFUL)
