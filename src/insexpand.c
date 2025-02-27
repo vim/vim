4ffc360544a94b1cb6ddcbb6e9298f982bcbdea7
Jim Zhou <csd_189@163.com>
2025-02-27 11:45:41 +0800

    Indentation fix

--- src/insexpand.c
+++ src/insexpand.c
@@ -2498,14 +2498,14 @@ ins_compl_stop(int c, int prev_mode, int retval)
     if (c == Ctrl_Y || (compl_enter_selects
 		    && (c == CAR || c == K_KENTER || c == NL)))
     {
-	    // If the popup menu is not displayed, the selection needs to be
-	    // inserted before further procceding.
-	    if (!pum_visible())
-		    ins_compl_insert(FALSE, FALSE);
-	    else
-		    word = vim_strsave(compl_shown_match->cp_str.string);
-	    retval = TRUE;
-	}
+	// If the popup menu is not displayed, the selection needs to be
+	// inserted before further procceding.
+	if (!pum_visible())
+	    ins_compl_insert(FALSE, FALSE);
+	else
+	    word = vim_strsave(compl_shown_match->cp_str.string);
+	retval = TRUE;
+    }
 
     // CTRL-E means completion is Ended, go back to the typed text.
     // but only do this, if the Popup is still visible
