50e0020f1e9c085025fda6856788030b57a61993
Christian Brabandt <cb@256bit.org>
2025-03-17 21:24:10 +0100

    Problem: wrong translation for encoding failures
    Problem:  wrong translation for encoding failures because of using
              literal "from" and "to" in the resulting error message
    Solution: use separate error messages for encoding errors "from" and
              "to" encoding.
    
    fixes: #16898
    
    Signed-off-by: Christian Brabandt <cb@256bit.org>

--- src/errors.h
+++ src/errors.h
@@ -3660,5 +3660,7 @@ EXTERN char e_winfixbuf_cannot_go_to_buffer[]
 	INIT(= N_("E1513: Cannot switch buffer. 'winfixbuf' is enabled"));
 EXTERN char e_invalid_return_type_from_findfunc[]
 	INIT(= N_("E1514: 'findfunc' did not return a List type"));
-EXTERN char e_str_encoding_failed[]
-	INIT(= N_("E1515: Unable to convert %s '%s' encoding"));
+EXTERN char e_str_encoding_from_failed[]
+	INIT(= N_("E1515: Unable to convert from '%s' encoding"));
+EXTERN char e_str_encoding_to_failed[]
+	INIT(= N_("E1516: Unable to convert to '%s' encoding"));
