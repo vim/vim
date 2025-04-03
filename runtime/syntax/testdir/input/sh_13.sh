6099db9a60d1c047bf9c8feee3e1689c4e653250
Doug Kearns <dougkearns@gmail.com>
2025-04-03 21:13:39 +0200

    runtime(sh): Update syntax file, command substitution opening paren at EOL
    Allow the opening parenthesis of a command substitution to appear at
    EOL.
    
    This fixes the issue raised in
    https://github.com/vim/vim/issues/17026#issuecomment-2774112284.
    
    closes: #17044
    
    Signed-off-by: Doug Kearns <dougkearns@gmail.com>
    Signed-off-by: Christian Brabandt <cb@256bit.org>

---
+++ runtime/syntax/testdir/input/sh_13.sh
@@ -0,0 +1,8 @@
+# Issue #17026 (bash highlighting requires space after $())
+# https://github.com/vim/vim/issues/17026#issuecomment-2774112284
+
+_comp_compgen_split -l -- "$(
+    tmux list-commands -F "#{command_list_name}"
+    tmux list-commands -F "#{command_list_alias}"
+)"
+
