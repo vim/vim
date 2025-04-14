443ea4044efcc68771f258a774a8ab1c041db311
girishji <girishji@gmail.com>
2025-04-14 13:45:19 +0200

    Update src/insexpand.c
    Co-authored-by: glepnir <glephunter@gmail.com>

--- src/insexpand.c
+++ src/insexpand.c
@@ -1368,7 +1368,6 @@ ins_compl_build_pum(void)
     int		compl_no_select = (cur_cot_flags & COT_NOSELECT) != 0;
     int		fuzzy_filter = (cur_cot_flags & COT_FUZZY) != 0;
     int		fuzzy_sort = fuzzy_filter && !(cur_cot_flags & COT_NOSORT);
-
     compl_T	*match_head = NULL;
     compl_T	*match_tail = NULL;
     compl_T	*match_next = NULL;
