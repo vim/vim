f698721f67dfdf7a35cfdd061b89a3f25764ddd3
basilisk0315 <basilisk@internode.on.net>
2025-04-23 09:26:56 +1000

    Remove non-related change

--- src/gui_w32.c
+++ src/gui_w32.c
@@ -3673,7 +3673,7 @@ gui_mch_exit(int rc UNUSED)
     static char_u *
 logfont2name(LOGFONTW lf)
 {
-    size_t	res_size;
+    char	*p;
     char	*res;
     char	*charset_name;
     char	*quality_name;
@@ -3686,46 +3686,43 @@ logfont2name(LOGFONTW lf)
     charset_name = charset_id2name((int)lf.lfCharSet);
     quality_name = quality_id2name((int)lf.lfQuality);
 
-    res_size = STRLEN(font_name) + 30
-		    + (charset_name == NULL ? 0 : STRLEN(charset_name) + 2)
-		    + (quality_name == NULL ? 0 : STRLEN(quality_name) + 2);
-    res = alloc(res_size);
+    res = alloc(strlen(font_name) + 30
+		    + (charset_name == NULL ? 0 : strlen(charset_name) + 2)
+		    + (quality_name == NULL ? 0 : strlen(quality_name) + 2));
     if (res != NULL)
     {
-	char	*p;
-	size_t	plen;
-
 	p = res;
 	// make a normal font string out of the lf thing:
 	points = pixels_to_points(
 			 lf.lfHeight < 0 ? -lf.lfHeight : lf.lfHeight, TRUE);
 	if (lf.lfWeight == FW_NORMAL || lf.lfWeight == FW_BOLD)
-	    plen = vim_snprintf_safelen(
-		(char *)p, res_size, "%s:h%d", font_name, points);
+	    sprintf((char *)p, "%s:h%d", font_name, points);
 	else
-	    plen = vim_snprintf_safelen(
-		(char *)p, res_size, "%s:h%d:W%ld", font_name, points, lf.lfWeight);
-
-	// replace spaces with underscores.
+	    sprintf((char *)p, "%s:h%d:W%ld", font_name, points, lf.lfWeight);
 	while (*p)
 	{
 	    if (*p == ' ')
 		*p = '_';
 	    ++p;
 	}
-
-	plen += vim_snprintf_safelen(
-	    (char *)p + plen,
-	    res_size,
-	    "%s%s%s%s",
-	    lf.lfItalic ? ":i" : "",
-	    lf.lfWeight ? ":b" : "",
-	    lf.lfUnderline ? ":u" : "",
-	    lf.lfStrikeOut ? ":s" : "");
+	if (lf.lfItalic)
+	    STRCAT(p, ":i");
+	if (lf.lfWeight == FW_BOLD)
+	    STRCAT(p, ":b");
+	if (lf.lfUnderline)
+	    STRCAT(p, ":u");
+	if (lf.lfStrikeOut)
+	    STRCAT(p, ":s");
 	if (charset_name != NULL)
-	    plen += vim_snprintf_safelen((char *)p + plen, res_size - plen, ":c%s", charset_name);
+	{
+	    STRCAT(p, ":c");
+	    STRCAT(p, charset_name);
+	}
 	if (quality_name != NULL)
-	    vim_snprintf((char *)p + plen, res_size - plen, ":q%s", quality_name);
+	{
+	    STRCAT(p, ":q");
+	    STRCAT(p, quality_name);
+	}
     }
 
     vim_free(font_name);
@@ -5054,7 +5051,7 @@ _OnMenuSelect(HWND hwnd, WPARAM wParam, LPARAM lParam)
 #endif
 
     static BOOL
-_OnGetDpiScaledSize(HWND hwnd UNUSED, UINT dpi, SIZE *size)
+_OnGetDpiScaledSize(HWND hwnd, UINT dpi, SIZE *size)
 {
     int		old_width, old_height;
     int		new_width, new_height;
