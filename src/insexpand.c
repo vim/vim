ceff50620158708dbe60542ae4170da1ddec8615
glepnir <glephunter@gmail.com>
2025-05-11 17:30:54 +0800

    fix: cycling to original text after last item
    Problem: Cannot return to the original text after selecting the next item when the currently selected item is the last one.
    
    Solution: When continuing to move down past the last item, locate the original completion at the head/tail nodes of the compl linked list

--- src/insexpand.c
+++ src/insexpand.c
@@ -5236,9 +5236,8 @@ find_comp_when_fuzzy(void)
 
     if ((is_forward && compl_selected_item == compl_match_arraysize - 1)
 	    || (is_backward && compl_selected_item == 0))
-	return compl_first_match != compl_shown_match ?
-	    (is_forward ? compl_shown_match->cp_next : compl_first_match) :
-	    (compl_first_match->cp_prev ? compl_first_match->cp_prev : NULL);
+	return match_at_original_text(compl_first_match)
+			    ? compl_first_match : compl_first_match->cp_prev;
 
     if (is_forward)
 	target_idx = compl_selected_item + 1;
