2c7ccaab87279b3f1a53b00fe64c3eed9fa89804
John Marriott <basilisk@internode.on.net>
2025-05-26 20:25:38 +0200

    patch 9.1.1414: MS-Windows: compile warnings in os_win32.c
    Problem:  MS-Windows: compile warnings in os_win32.c
    Solution: update function declarations, correctly access param arg in
              set_flag() using (sig_atomic_t) cast (John Marriott)
    
    closes: #17368
    
    Co-authored-by: Hirohito Higashi <h.east.727@gmail.com>
    Signed-off-by: John Marriott <basilisk@internode.on.net>
    Signed-off-by: Christian Brabandt <cb@256bit.org>

--- src/os_win32.c
+++ src/os_win32.c
@@ -175,12 +175,12 @@ static int conpty_working = 0;
 static int conpty_type = 0;
 static int conpty_stable = 0;
 static int conpty_fix_type = 0;
-static void vtp_flag_init();
+static void vtp_flag_init(void);
 
 #if !defined(FEAT_GUI_MSWIN) || defined(VIMDLL)
 static int vtp_working = 0;
-static void vtp_init();
-static void vtp_exit();
+static void vtp_init(void);
+static void vtp_exit(void);
 static void vtp_sgr_bulk(int arg);
 static void vtp_sgr_bulks(int argc, int *argv);
 
@@ -8994,9 +8994,7 @@ static sig_atomic_t *timeout_flag = &timeout_flags[0];
     static void CALLBACK
 set_flag(void *param, BOOLEAN unused2 UNUSED)
 {
-    int *timeout_flag = (int *)param;
-
-    *timeout_flag = TRUE;
+    *(sig_atomic_t *)param = TRUE;
 }
 
 /*
