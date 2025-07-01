b84ab0288f36145b4e9c095f68bf673ad6f69cd4
Damien Lejay <damien@lejay.be>
2025-07-01 21:57:59 +0200

    patch 9.1.1500: if_python: typo in python error variable
    Problem:  if_python: typo in python error variable
    Solution: fix typo (Damien Lejay)
    
    closes: #17640
    
    Signed-off-by: Damien Lejay <damien@lejay.be>
    Signed-off-by: Christian Brabandt <cb@256bit.org>

--- src/errors.h
+++ src/errors.h
@@ -2303,7 +2303,7 @@ EXTERN char e_cant_rename_viminfo_file_to_str[]
 	INIT(= N_("E886: Can't rename viminfo file to %s!"));
 #endif
 #if defined(FEAT_PYTHON) || defined(FEAT_PYTHON3)
-EXTERN char e_sorry_this_command_is_disabled_python_side_module_could_not_be_loaded[]
+EXTERN char e_sorry_this_command_is_disabled_python_site_module_could_not_be_loaded[]
 	INIT(= N_("E887: Sorry, this command is disabled, the Python's site module could not be loaded."));
 #endif
 EXTERN char e_nfa_regexp_cannot_repeat_str[]
