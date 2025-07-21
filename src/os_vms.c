f0bcc157b27d80542ebb77fe056ce4495ec86658
Zoltan Arpadffy <zoltan.arpadffy@gmail.com>
2025-07-21 07:20:40 +0200

    try to fix the invalid indents

--- src/os_vms.c
+++ src/os_vms.c
@@ -990,43 +990,40 @@ vms_init(void)
     int sts;
 
     /* Loop through all items in the decc_feat_array[]. */
-
     for (i = 0; decc_feat_array[i].name != NULL; i++)
     {
-        /* Get the feature index. */
-        feat_index = decc$feature_get_index(decc_feat_array[i].name);
-        if (feat_index >= 0)
-        {
-            /* Valid item.  Collect its properties. */
-            feat_value = decc$feature_get_value(feat_index, 1);
-            feat_value_min = decc$feature_get_value(feat_index, 2);
-            feat_value_max = decc$feature_get_value(feat_index, 3);
+	/* Get the feature index. */
+	feat_index = decc$feature_get_index(decc_feat_array[i].name);
+	if (feat_index >= 0)
+	{
+	    /* Valid item.  Collect its properties. */
+	    feat_value = decc$feature_get_value(feat_index, 1);
+	    feat_value_min = decc$feature_get_value(feat_index, 2);
+	    feat_value_max = decc$feature_get_value(feat_index, 3);
 
-            if ((decc_feat_array[i].value >= feat_value_min) &&
-                (decc_feat_array[i].value <= feat_value_max))
-            {
-                /* Valid value.  Set it if necessary. */
-                if (feat_value != decc_feat_array[i].value)
-                {
-                    sts = decc$feature_set_value(feat_index, 1,
-                                                 decc_feat_array[i].value);
-                }
-            }
-            else
-            {
-                /* Invalid DECC feature value. */
-                printf(" INVALID DECC FEATURE VALUE, %d: %d <= %s <= %d.\n",
-                       feat_value, feat_value_min, decc_feat_array[i].name,
-                       feat_value_max);
-            }
-        }
-        else
-        {
-            /* Invalid DECC feature name. */
-            printf(" UNKNOWN DECC FEATURE: %s.\n", decc_feat_array[i].name);
-        }
+	    if ((decc_feat_array[i].value >= feat_value_min) && (decc_feat_array[i].value <= feat_value_max))
+	    {
+		/* Valid value.  Set it if necessary. */
+		if (feat_value != decc_feat_array[i].value)
+		{
+		    sts = decc$feature_set_value(feat_index, 1, decc_feat_array[i].value);
+		}
+	    }
+	    else
+	    {
+		/* Invalid DECC feature value. */
+		printf("INVALID DECC FEATURE VALUE, %d: %d <= %s <= %d.\n", 
+			feat_value, feat_value_min, decc_feat_array[i].name, feat_value_max);
+	    }
+	}
+	else
+	{
+	    /* Invalid DECC feature name. */
+	    printf("UNKNOWN DECC FEATURE: %s.\n", decc_feat_array[i].name);
+	}
     }
 }
+
 
 /* Get "vms_init()" into a valid, loaded LIB$INITIALIZE PSECT. */
 
