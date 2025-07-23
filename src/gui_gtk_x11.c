48a3b146b49d508f1d592974105c690b7af62ea8
Christoffer Aasted <chr.aasted@gmail.com>
2025-07-23 21:05:51 +0200

    patch 9.1.1585: Wayland: gvim still needs GVIM_ENABLE_WAYLAND
    Problem:  Wayland: gvim still needs GVIM_ENABLE_WAYLAND
    Solution: Drop the GVIM_ENABLE_WAYLAND code, always enable both X11 and
              Wayland GUI support (Christoffer Aasted)
    
    closes: #17817
    
    Signed-off-by: Christoffer Aasted <chr.aasted@gmail.com>
    Signed-off-by: Christian Brabandt <cb@256bit.org>

--- src/gui_gtk_x11.c
+++ src/gui_gtk_x11.c
@@ -1732,15 +1732,6 @@ gui_mch_init_check(void)
     }
 #endif
 
-#if GTK_CHECK_VERSION(3,10,0)
-    // Vim currently assumes that Gtk means X11, so it cannot use native Gtk
-    // support for other backends such as Wayland.
-    //
-    // Use an environment variable to enable unfinished Wayland support.
-    if (getenv("GVIM_ENABLE_WAYLAND") == NULL)
-	gdk_set_allowed_backends ("x11");
-#endif
-
 #ifdef FEAT_GUI_GNOME
     if (gtk_socket_id == 0)
 	using_gnome = 1;
