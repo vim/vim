8e2a2291892935f28645cbe550ef2856bab1ee4e
Robert Muir <rmuir@apache.org>
2025-08-10 09:19:36 +0200

    patch 9.1.1618: completion: incorrect selected index returned from complete_info()
    Problem:  completion: incorrect selected index returned from
              complete_info()
    Solution: Return the index into "items" and restore the previous
              behaviour (Robert Muir).
    
    complete_info() returned an incorrect selected index after
    0ac1eb3555445f4c458c06cef7c411de1c8d1020 (Patch v9.1.1311). Effectively
    it became an index into "matches" instead of "items". Return the index
    into "items" by default to restore the previous behavior, unless
    "matches" was requested.
    
    closes: #17952
    
    Co-authored-by: Hirohito Higashi <h.east.727@gmail.com>
    Signed-off-by: Robert Muir <rmuir@apache.org>
    Signed-off-by: Christian Brabandt <cb@256bit.org>

--- src/insexpand.c
+++ src/insexpand.c
@@ -4184,8 +4184,8 @@ get_complete_info(list_T *what_list, dict_T *retdict)
 		    if (compl_curr_match != NULL
 			    && compl_curr_match->cp_number == match->cp_number)
 			selected_idx = list_idx;
-		    if (match->cp_in_match_array)
-			list_idx += 1;
+		    if (!has_matches || match->cp_in_match_array)
+			list_idx++;
 		}
 		match = match->cp_next;
 	    }
