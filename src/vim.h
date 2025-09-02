1c36a852378ee9d4ab871d44d12db7e0277b6371
Damien Lejay <damien@lejay.be>
2025-09-02 19:18:21 +0200

    patch 9.1.1727: Nextstep support still included
    Problem:  Nextstep support still included
    Solution: Deprecate Nextstep code and undef corresponding feature flag
    
    closes: #18131
    
    Signed-off-by: Damien Lejay <damien@lejay.be>
    Signed-off-by: Christian Brabandt <cb@256bit.org>

--- src/vim.h
+++ src/vim.h
@@ -40,6 +40,22 @@
 #  error configure did not run properly.  Check auto/config.log.
 # endif
 
+/*
+ * NeXTSTEP / OPENSTEP support deprecation
+ *
+ * NeXT hardware was discontinued in 1993, and the last OPENSTEP release
+ * (4.2) shipped in 1996–1997. No known users remain today.
+ *
+ * To simplify maintenance, NeXT support is formally deprecated. If you hit
+ * this error, please report it to the Vim maintainers.
+ *
+ * This guard will be removed once the remaining NeXT-specific code paths
+ * are deleted in a future release.
+ */
+#if defined(NeXT) || defined(__NeXT__)
+# error "NeXTSTEP / OPENSTEP support has been deprecated."
+#endif
+
 # if (defined(__linux__) && !defined(__ANDROID__)) || defined(__CYGWIN__) || defined(__GNU__)
 // Needed for strptime().  Needs to be done early, since header files can
 // include other header files and end up including time.h, where these symbols
