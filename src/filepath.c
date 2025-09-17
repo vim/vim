ed85209572cd2938d56ea6501811bf479313006e
Damien Lejay <damien@lejay.be>
2025-09-17 12:24:29 +0200

    fix
    Signed-off-by: Damien Lejay <damien@lejay.be>

--- src/filepath.c
+++ src/filepath.c
@@ -941,11 +941,11 @@ f_exepath(typval_T *argvars, typval_T *rettv)
     char_u *p = NULL;
 
     rettv->v_type = VAR_STRING;
-    rettv->vval.v_string = p;
 
     if (in_vim9script() && check_for_nonempty_string_arg(argvars, 0) == FAIL)
 	return;
     (void)mch_can_exe(tv_get_string(&argvars[0]), &p, TRUE);
+    rettv->vval.v_string = p;
 }
 
 /*
