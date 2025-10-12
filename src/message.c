fcf4c435af6fe00348506860c1433414d35223a7
bfoersterling <bjoern.foersterling@gmail.com>
2025-10-12 14:26:34 +0000

    patch 9.1.1849: CTRL-F and CTRL-B don't work in more prompt
    Problem:  CTRL-F and CTRL-B don't work in more prompt
    Solution: Make CTRL-F and CTRL-B scroll by a screen down/up
              (Bjoern Foersterling)
    
    closes: #18545
    
    Signed-off-by: bfoersterling <bjoern.foersterling@gmail.com>
    Signed-off-by: Christian Brabandt <cb@256bit.org>

--- src/message.c
+++ src/message.c
@@ -1356,7 +1356,7 @@ wait_return(int redraw)
 		*/
 		if (p_more && !p_cp)
 		{
-		    if (c == 'b' || c == 'k' || c == 'u' || c == 'g'
+		    if (c == 'b' || c == Ctrl_B || c == 'k' || c == 'u' || c == 'g'
 						|| c == K_UP || c == K_PAGEUP)
 		    {
 			if (msg_scrolled > Rows)
@@ -1385,7 +1385,7 @@ wait_return(int redraw)
 			}
 		    }
 		    else if (msg_scrolled > Rows - 2
-			    && (c == 'j' || c == 'd' || c == 'f'
+			    && (c == 'j' || c == 'd' || c == 'f' || c == Ctrl_F
 					    || c == K_DOWN || c == K_PAGEDOWN))
 			c = K_IGNORE;
 		}
@@ -3318,12 +3318,14 @@ do_more_prompt(int typed_char)
 	    break;
 
 	case 'b':		// one page back
+	case Ctrl_B:
 	case K_PAGEUP:
 	    toscroll = -(Rows - 1);
 	    break;
 
 	case ' ':		// one extra page
 	case 'f':
+	case Ctrl_F:
 	case K_PAGEDOWN:
 	case K_LEFTMOUSE:
 	    toscroll = Rows - 1;
