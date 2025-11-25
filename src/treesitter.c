58eabe04fe00cc29c93bd5f3555f8da2c00db73b
Foxe Chen <chen.foxe@gmail.com>
2025-11-25 18:18:15 -0500

    initial commit

---
+++ src/treesitter.c
@@ -0,0 +1,22 @@
+/* vi:set ts=8 sts=4 sw=4 noet:
+ *
+ * VIM - Vi IMproved	by Bram Moolenaar
+ *
+ * Do ":help uganda"  in Vim to read copying and usage conditions.
+ * Do ":help credits" in Vim to see a list of people who contributed.
+ * See README.txt for an overview of the Vim source code.
+ */
+
+/*
+ * treesitter.c: Treesitter integration logic
+ */
+
+#include "vim.h"
+
+#ifdef FEAT_TREESITTER
+
+#include <tree_sitter/api.h>
+
+
+
+#endif // FEAT_TREESITTER
