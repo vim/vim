01b94b18195d53756603608f9f21b9e941f0a6a6
Foxe Chen <chen.foxe@gmail.com>
2025-11-26 03:08:40 -0500

    iniitial parsing backend logic

--- src/treesitter.h
+++ src/treesitter.h
@@ -13,30 +13,47 @@
 
 #ifdef FEAT_TREESITTER
 
+#include "vim.h"
 #include <tree_sitter/api.h>
 
-typedef struct
+typedef struct vts_tree_S vts_tree_T;
+typedef struct vts_node_S vts_node_T;
+typedef struct vts_parser_S vts_parser_T;
+
+struct vts_tree_S
 {
     int			tt_refcount;
     TSTree	    	*tt_tree;
-} vts_tree_T;
+}_tree_T;
 
-typedef struct
+struct vts_node_S
 {
     int			tn_refcount;
     vts_tree_T	    	*tn_tree;	// Parent TSTree object, we hold our own
 					// reference.
     TSNode	    	*tn_node;
-} vts_node_T;
+};
 
 // Represents a TSParser object. Each object may have child parsers, essentially
 // creating a tree of parsers. This allows us to handle injected languages
 // inside a language.
-typedef struct
+struct vts_parser_S
 {
     TSParser	    	*tp_parser;
     vts_tree_T	    	*tp_tree;	// Associated TSTree object
+    TSInput		tp_input;
+    bool		tp_parsing;	// true if currently parsing
+
+#ifdef ELAPSED_FUNC
+    elapsed_T		tp_elapsed;	// Used check if a parse operation
+					// should be cancelled.
+#endif
+    long		tp_timeout;
+
     garray_T	    	tp_children;
-} vts_parser_T;
+    
+    // Used for async parsing
+    vts_parser_T	*tp_next;
+};
 
 #endif // FEAT_TREESITTER
