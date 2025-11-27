cbcbff871224115c45bbd14582749a487c6fad30
Girish Palya <girishji@gmail.com>
2025-11-27 21:19:54 +0000

    patch 9.1.1933: completion: complete_match() is not useful
    Problem:  completion: complete_match() Vim script function and
              'isexpand' option are not that useful and confusing
              (after v9.1.1341)
    Solution: Remove function and option and clean up code and documentation
              (Girish Palya).
    
    complete_match() and 'isexpand' add no real functionality to Vim. They
    duplicate what `strridx()` already does, yet pretend to be part of the
    completion system. They have nothing to do with the completion mechanism.
    
    * `f_complete_match()` in `insexpand.c` does not call any completion code.
       It’s just a `STRNCMP()` wrapper with fluff logic.
    * `'isexpand'` exists only as a proxy argument to that function.
       It does nothing on its own and amounts to misuse of a new option.
    
    The following Vim script function can be used to implement the same
    functionality:
    
    ```vim
      func CompleteMatch(triggers, sep=',')
        let line = getline('.')->strpart(0, col('.') - 1)
        let result = []
        for trig in split(a:triggers, a:sep)
          let idx = strridx(line, trig)
          if l:idx >= 0
            call add(result, [idx + 1, trig])
          endif
        endfor
        return result
      endfunc
    ```
    
    related: #16716
    fixes: #18563
    closes: #18790
    
    Signed-off-by: Girish Palya <girishji@gmail.com>
    Signed-off-by: Christian Brabandt <cb@256bit.org>

--- src/option.c
+++ src/option.c
@@ -6508,9 +6508,6 @@ unset_global_local_option(char_u *name, void *from)
 	    clear_string_option(&buf->b_p_cot);
 	    buf->b_cot_flags = 0;
 	    break;
-	case PV_ISE:
-	    clear_string_option(&buf->b_p_ise);
-	    break;
 	case PV_DICT:
 	    clear_string_option(&buf->b_p_dict);
 	    break;
@@ -6639,7 +6636,6 @@ get_varp_scope(struct vimoption *p, int scope)
 	    case PV_INC:  return (char_u *)&(curbuf->b_p_inc);
 #endif
 	    case PV_COT:  return (char_u *)&(curbuf->b_p_cot);
-	    case PV_ISE:  return (char_u *)&(curbuf->b_p_ise);
 	    case PV_DICT: return (char_u *)&(curbuf->b_p_dict);
 #ifdef FEAT_DIFF
 	    case PV_DIA:  return (char_u *)&(curbuf->b_p_dia);
@@ -6727,8 +6723,6 @@ get_varp(struct vimoption *p)
 #endif
 	case PV_COT:	return *curbuf->b_p_cot != NUL
 				    ? (char_u *)&(curbuf->b_p_cot) : p->var;
-	case PV_ISE:	return *curbuf->b_p_ise != NUL
-				    ? (char_u *)&(curbuf->b_p_ise) : p->var;
 	case PV_DICT:	return *curbuf->b_p_dict != NUL
 				    ? (char_u *)&(curbuf->b_p_dict) : p->var;
 #ifdef FEAT_DIFF
@@ -7579,7 +7573,6 @@ buf_copy_options(buf_T *buf, int flags)
 	    buf->b_p_dia = empty_option;
 #endif
 	    buf->b_p_tsr = empty_option;
-	    buf->b_p_ise = empty_option;
 #ifdef FEAT_COMPL_FUNC
 	    buf->b_p_tsrfu = empty_option;
 #endif
