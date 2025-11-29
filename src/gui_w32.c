86c6a05c03bc99bbc6ced8a6bfd64a2fb331285a
Mao-Yining <mao.yining@outlook.com>
2025-11-29 22:10:53 +0800

    feat: add fullscreen mode support for Windows GUI
    - Implement fullscreen mode controlled by the 'go-s' flag in 'guioptions'
    - Note: 'go-s' flag cannot be set prior to window initialization due to
      timing constraints in the GUI loading process
    - Update documentation with usage examples and platform-specific details
    
    See :help 'go-s' and :help gui-w32-fullscreen for complete documentation.
    
    Signed-off-by: Mao-Yining <mao.yining@outlook.com>

--- src/gui_w32.c
+++ src/gui_w32.c
@@ -3117,6 +3117,66 @@ gui_mch_set_curtab(int nr)
 #endif
 
 /*
+ * When flag is true, set fullscreen on.
+ * When flag is false, set fullscreen off.
+ *
+ * Note: Each state transition (enter/exit fullscreen) should only be called
+ * once before the opposite transition.
+ */
+    void
+gui_mch_set_fullscreen(int flag)
+{
+    static RECT normal_rect;
+    static LONG_PTR normal_style, normal_exstyle;
+    HMONITOR	mon;
+    MONITORINFO	moninfo;
+    RECT	rc;
+
+    if (flag)
+    {
+	// Enter fullscreen mode
+	GetWindowRect(s_hwnd, &rc);
+
+	moninfo.cbSize = sizeof(MONITORINFO);
+	mon = MonitorFromRect(&rc, MONITOR_DEFAULTTONEAREST);
+	if (mon == NULL || !GetMonitorInfo(mon, &moninfo))
+	    return;
+
+	// Save current window state
+	GetWindowRect(s_hwnd, &normal_rect);
+	normal_style = GetWindowLongPtr(s_hwnd, GWL_STYLE);
+	normal_exstyle = GetWindowLongPtr(s_hwnd, GWL_EXSTYLE);
+
+	// Set fullscreen styles
+	SetWindowLongPtr(s_hwnd, GWL_STYLE,
+		normal_style & ~(WS_CAPTION | WS_THICKFRAME));
+	SetWindowLongPtr(s_hwnd, GWL_EXSTYLE,
+		normal_exstyle & ~(WS_EX_DLGMODALFRAME | WS_EX_WINDOWEDGE |
+		    WS_EX_CLIENTEDGE | WS_EX_STATICEDGE));
+	SetWindowPos(s_hwnd, NULL,
+		moninfo.rcMonitor.left,
+		moninfo.rcMonitor.top,
+		moninfo.rcMonitor.right - moninfo.rcMonitor.left,
+		moninfo.rcMonitor.bottom - moninfo.rcMonitor.top,
+		SWP_NOZORDER | SWP_NOACTIVATE | SWP_FRAMECHANGED);
+    }
+    else
+    {
+	// Exit fullscreen mode
+	SetWindowLongPtr(s_hwnd, GWL_STYLE, normal_style);
+	SetWindowLongPtr(s_hwnd, GWL_EXSTYLE, normal_exstyle);
+
+	// Restore original window position and size
+	SetWindowPos(s_hwnd, NULL,
+		normal_rect.left,
+		normal_rect.top,
+		normal_rect.right - normal_rect.left,
+		normal_rect.bottom - normal_rect.top,
+		SWP_NOZORDER | SWP_NOACTIVATE | SWP_FRAMECHANGED);
+    }
+}
+
+/*
  * ":simalt" command.
  */
     void
