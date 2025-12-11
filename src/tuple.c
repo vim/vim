6da1966f069508e3b5fe76232f59e314ff1caa37
Foxe Chen <chen.foxe@gmail.com>
2025-12-11 02:09:53 -0500

    add tsquery_inspect() function

--- src/tuple.c
+++ src/tuple.c
@@ -87,6 +87,54 @@ tuple_set_item(tuple_T *tuple, int idx, typval_T *tv)
 }
 
 /*
+ * Set a tuple "idx" for a tuple previously allocated with
+ * tuple_alloc_with_items().
+ * Each item must be set exactly once.
+ * The reference count of the given tuple is not incremented.
+ */
+    void
+tuple_set_tuple(tuple_T *tuple, int idx, tuple_T *t)
+{
+    typval_T *tv = TUPLE_ITEM(tuple, idx);
+
+    tv->v_type = VAR_TUPLE;
+    tv->vval.v_tuple = t;
+    tuple->tv_items.ga_len++;
+}
+
+/*
+ * Set a list "idx" for a tuple previously allocated with
+ * tuple_alloc_with_items().
+ * Each item must be set exactly once.
+ * The reference count of the given list is not incremented.
+ */
+    void
+tuple_set_list(tuple_T *tuple, int idx, list_T *l)
+{
+    typval_T *tv = TUPLE_ITEM(tuple, idx);
+
+    tv->v_type = VAR_LIST;
+    tv->vval.v_list = l;
+    tuple->tv_items.ga_len++;
+}
+
+/*
+ * Set a tuple "idx" for a tuple previously allocated with
+ * tuple_alloc_with_items().
+ * Each item must be set exactly once.
+ * The string is not copied
+ */
+    void
+tuple_set_string(tuple_T *tuple, int idx, char_u *str)
+{
+    typval_T *tv = TUPLE_ITEM(tuple, idx);
+
+    tv->v_type = VAR_STRING;
+    tv->vval.v_string = str;
+    tuple->tv_items.ga_len++;
+}
+
+/*
  * Allocate an empty tuple for a return value, with reference count set.
  * Returns OK or FAIL.
  */
