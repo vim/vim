57b577df3217b4f28abff6fe3c0a480192927ae2
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2026-01-30 09:45:55 +0000

    patch 9.1.2116: crash when using 'tagfunc'
    Problem:  crash when using 'tagfunc' (Rodrigo Queipo)
    Solution: Do not add the user_data to the 'info' dictionary when called
              for insert mode completion (the TAG_INS_COMP flag is set).
              Completion should not depend on the state of a previous tag
              jump. (Yasuhiro Matsumoto)
    
    fixes:  #19255
    closes: #19284
    
    Signed-off-by: Yasuhiro Matsumoto <mattn.jp@gmail.com>
    Signed-off-by: Christian Brabandt <cb@256bit.org>

--- src/tag.c
+++ src/tag.c
@@ -1460,7 +1460,7 @@ find_tagfunc_tags(
     // create 'info' dict argument
     if ((d = dict_alloc_lock(VAR_FIXED)) == NULL)
 	return FAIL;
-    if (tag->user_data != NULL)
+    if (!(flags & TAG_INS_COMP) && tag->user_data != NULL)
 	dict_add_string(d, "user_data", tag->user_data);
     if (buf_ffname != NULL)
 	dict_add_string(d, "buf_ffname", buf_ffname);
