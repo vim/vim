e9700557fe1e00b35fe05b40f7c3b1b492d127a6
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2026-03-07 23:40:28 +0900

    Fix memory leak

--- src/window.c
+++ src/window.c
@@ -2522,7 +2522,9 @@ win_init_empty(win_T *wp)
     wp->w_prev_pcmark.lnum = 0;
     wp->w_prev_pcmark.col = 0;
     wp->w_topline = 1;
-    wp->w_hl = NULL;
+    remove_highlight_overrides(wp->w_hl);
+    VIM_CLEAR(wp->w_hl);
+    wp->w_hl_len = 0;
 #ifdef FEAT_DIFF
     wp->w_topfill = 0;
 #endif
