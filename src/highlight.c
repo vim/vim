49a5a804106dcaec237478cee12f1ca8f35352c9
Foxe Chen <chen.foxe@gmail.com>
2026-03-11 19:37:26 +0000

    patch 9.2.0138: winhighlight option handling can be improved
    Problem:  winhighlight option handling can be improved
              (after: v9.2.0093)
    Solution: Check entire override stack in update_highlight_overrides();
              update w_hl of every window in highlight_changed() (Foxe Chen).
    
    closes: #19633
    
    Signed-off-by: Foxe Chen <chen.foxe@gmail.com>
    Signed-off-by: Christian Brabandt <cb@256bit.org>

--- src/highlight.c
+++ src/highlight.c
@@ -4371,8 +4371,14 @@ highlight_changed(void)
 	}
     }
 
+    // Highlight ids may have been changed, so keep windows up to date
     FOR_ALL_WINDOWS(wp)
-	wp->w_hlfwin_id = hlf_get_id(wp, HLF_WIN);
+    {
+	char *errmsg = update_winhighlight(wp, wp->w_p_whl);
+
+	if (errmsg != NULL)
+	    emsg(_(errmsg));
+    }
 
 #ifdef FEAT_TERMINAL
     term_update_hlfwin_all();
@@ -5480,7 +5486,6 @@ update_highlight_overrides(hl_override_T *old, hl_override_T *hl_new, int newlen
 	{
 	    set->arr = hl_new;
 	    set->len = newlen;
-	    break;
 	}
     }
 }
