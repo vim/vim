b2c49d22a0de9a5614f097d0ef34ad8f92fb0173
lilydjwg <lilydjwg@gmail.com>
2026-04-23 22:02:44 +0800

    fix GVim does not lose selection ownership and does not use VisualNOS when it should
    fixes #19914.

--- src/drawline.c
+++ src/drawline.c
@@ -1465,7 +1465,7 @@ win_line(
 		area_highlighting = TRUE;
 		vi_attr = HL_ATTR(HLF_V);
 #if defined(FEAT_CLIPBOARD) && defined(FEAT_X11)
-		if (X_DISPLAY &&
+		if ((clipmethod != CLIPMETHOD_PROVIDER) &&
 			((clip_star.available && !clip_star.owned
 						    && clip_isautosel_star())
 			    || (clip_plus.available && !clip_plus.owned
