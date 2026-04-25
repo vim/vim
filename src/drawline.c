479cc581d9ed29dd89a1afa15c8749bf3afc9526
Shane Harper <shane@shaneharper.net>
2026-04-25 20:35:46 +1000

    Problem: FEAT_X11 required to use VisualNOS.
    Vim will now be able to use VisualNOS when built with Wayland clipboard support but without FEAT_X11. (Fixes a problem that has existed since Wayland support was added to Vim.)

--- src/drawline.c
+++ src/drawline.c
@@ -1464,7 +1464,7 @@ win_line(
 	    {
 		area_highlighting = TRUE;
 		vi_attr = HL_ATTR(HLF_V);
-#if defined(FEAT_CLIPBOARD) && defined(FEAT_X11)
+#if defined(FEAT_CLIPBOARD) && (defined(FEAT_X11) || defined(FEAT_WAYLAND_CLIPBOARD))
 		if ((clip_star.available && !clip_star.owned
 						    && clip_isautosel_star())
 			|| (clip_plus.available && !clip_plus.owned
