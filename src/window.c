bd0f3e6da5df70ab2250264d0a7efc7db83c3dc8
Luuk van Baal <luukvbaal@gmail.com>
2026-04-28 21:09:45 +0000

    patch 9.2.0413: Scrolling wrong with 'splitkeep' when changing 'cmdheight'
    Problem:  Cursor is not adjusted when 'cmdheight' is changed to cover
              the cursor with 'splitkeep' ~= "cursor".
    Solution: Handle window resize for 'splitkeep' after changing 'cmdheight'.
              Ensure previous window height is set when changing 'splitkeep'
              (Luuk van Baal).
    
    closes: #20043
    
    Signed-off-by: Luuk van Baal <luukvbaal@gmail.com>
    Signed-off-by: Christian Brabandt <cb@256bit.org>

--- src/window.c
+++ src/window.c
@@ -7591,6 +7591,7 @@ command_height(void)
     }
     if (p_ch < old_p_ch && command_frame_height && frp != NULL)
 	frame_add_height(frp, (int)(old_p_ch - p_ch));
+    win_fix_scroll(true);
 
     // Recompute window positions.
     win_comp_pos();
