381ff209618fffaab69a72bacb0d0c6fa8742edb
Christoffer Aasted <dezzadk@gmail.com>
2026-05-28 07:08:09 +0200

    fix(gtk3-wayland): crash right mouse-button tabline
    GtkNotebook (tabline) is a windowless container widget causing a
    nullptr deref inside `gdk_window_get_effective_parent()` as Wayland
    lacks a surface to anchor to.
    
    Thus use `gui.mainwin` and get coords.
    
    Closes: #18864

--- src/gui_gtk_x11.c
+++ src/gui_gtk_x11.c
@@ -3478,6 +3478,17 @@ on_tabline_menu(GtkWidget *widget, GdkEvent *event)
 	if (bevent->button == 3)
 	{
 # if GTK_CHECK_VERSION(3,22,2)
+#  ifdef GDK_WINDOWING_WAYLAND
+    if (gui.is_wayland)
+    {
+      int x, y;
+      gui_gtk_get_pointer(gui.mainwin, &x, &y, NULL);
+      gtk_menu_popup_at_rect(GTK_MENU(widget),
+          gtk_widget_get_window(gui.mainwin), &(GdkRectangle){x, y, 1, 1},
+          GDK_GRAVITY_SOUTH_WEST, GDK_GRAVITY_NORTH_WEST, NULL);
+    }
+    else
+#  endif
 	    gtk_menu_popup_at_pointer(GTK_MENU(widget), event);
 # else
 	    gtk_menu_popup(GTK_MENU(widget), NULL, NULL, NULL, NULL,
