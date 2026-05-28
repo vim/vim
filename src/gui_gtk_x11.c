4b1c475becf942b70c34d41ab487a264624d063d
Christoffer Aasted <dezzadk@gmail.com>
2026-05-28 07:37:50 +0200

    fix: shadow var

--- src/gui_gtk_x11.c
+++ src/gui_gtk_x11.c
@@ -3481,10 +3481,10 @@ on_tabline_menu(GtkWidget *widget, GdkEvent *event)
 #  ifdef GDK_WINDOWING_WAYLAND
     if (gui.is_wayland)
     {
-      int x, y;
-      gui_gtk_get_pointer(gui.mainwin, &x, &y, NULL);
+      int x2, y2;
+      gui_gtk_get_pointer(gui.mainwin, &x2, &y2, NULL);
       gtk_menu_popup_at_rect(GTK_MENU(widget),
-          gtk_widget_get_window(gui.mainwin), &(GdkRectangle){x, y, 1, 1},
+          gtk_widget_get_window(gui.mainwin), &(GdkRectangle){x2, y2, 1, 1},
           GDK_GRAVITY_SOUTH_WEST, GDK_GRAVITY_NORTH_WEST, NULL);
     }
     else
