fdc0d00f598229537c1d49bf100f0862fdd810e4
K.Takata <kentkt@csc.jp>
2026-06-04 18:23:09 +0900

    Remove unnecessary casts

--- src/GvimExt/gvimext.cpp
+++ src/GvimExt/gvimext.cpp
@@ -959,16 +959,14 @@ BOOL CShellExt::LoadMenuIcon()
 searchpath(char *name)
 {
     static char widename[2 * BUFSIZE];
-    static char location[2 * BUFSIZE + 2];
+    WCHAR location[BUFSIZE + 1];
 
     // There appears to be a bug in FindExecutableA() on Windows NT.
     // Use FindExecutableW() instead...
-    MultiByteToWideChar(CP_ACP, 0, (LPCSTR)name, -1,
-	    (LPWSTR)widename, BUFSIZE);
-    if (FindExecutableW((LPCWSTR)widename, (LPCWSTR)"",
-		(LPWSTR)location) > (HINSTANCE)32)
+    MultiByteToWideChar(CP_ACP, 0, name, -1, (LPWSTR)widename, BUFSIZE);
+    if (FindExecutableW((LPCWSTR)widename, L"", location) > (HINSTANCE)32)
     {
-	WideCharToMultiByte(CP_ACP, 0, (LPWSTR)location, -1,
+	WideCharToMultiByte(CP_ACP, 0, location, -1,
 		(LPSTR)widename, 2 * BUFSIZE, NULL, NULL);
 	return widename;
     }
