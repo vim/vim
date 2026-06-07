3def3b3777764c72eb53591b08f58b19ff41ebd6
Foxe Chen <chen.foxe@gmail.com>
2026-06-06 23:16:38 -0400

    fix gui_mch_insert_lines()

--- src/gui_gtk4.c
+++ src/gui_gtk4.c
@@ -1590,10 +1590,10 @@ gui_mch_insert_lines(int row, int num_lines)
 	    FILL_X(gui.scroll_region_left), FILL_Y(row + num_lines),
 	    FILL_X(gui.scroll_region_left), FILL_Y(row),
 	    gui.char_width * ncols + 1, gui.char_height * src_nrows);
+#endif
     gui_clear_block(
 	    row, gui.scroll_region_left,
 	    row + num_lines - 1, gui.scroll_region_right);
-#endif
 
     gtk_widget_queue_draw(gui.drawarea);
 }
