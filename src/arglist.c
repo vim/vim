90387c12388e7da6755611758f1281e2fa663f24
zeertzjq <zeertzjq@outlook.com>
2026-07-04 08:08:01 +0800

    :argdelete with pattern leads to wrong argidx()
    Problem:  :argdelete with pattern leads to wrong argidx().
    Solution: Correct argidx() in both branches of ex_argdelete().  Also use
              ARGCOUNT macro in two more places.
    
    related: patch 7.4.1119
    related: neovim/neovim#40564

--- src/arglist.c
+++ src/arglist.c
@@ -429,7 +429,7 @@ arglist_del_files(garray_T *alist_ga)
 		vim_free(ARGLIST[match].ae_fname);
 		mch_memmove(ARGLIST + match, ARGLIST + match + 1,
 			(ARGCOUNT - match - 1) * sizeof(aentry_T));
-		--ALIST(curwin)->al_ga.ga_len;
+		--ARGCOUNT;
 		if (curwin->w_arg_idx > match)
 		    --curwin->w_arg_idx;
 		--match;
@@ -900,33 +900,37 @@ ex_argdelete(exarg_T *eap)
 	    eap->line2 = ARGCOUNT;
 	n = eap->line2 - eap->line1 + 1;
 	if (*eap->arg != NUL)
+	{
 	    // Can't have both a range and an argument.
 	    emsg(_(e_invalid_argument));
-	else if (n <= 0)
+	    return;
+	}
+	if (n <= 0)
 	{
 	    // Don't give an error for ":%argdel" if the list is empty.
 	    if (eap->line1 != 1 || eap->line2 != 0)
 		emsg(_(e_invalid_range));
+	    return;
 	}
-	else
-	{
-	    for (i = eap->line1; i <= eap->line2; ++i)
-		vim_free(ARGLIST[i - 1].ae_fname);
-	    mch_memmove(ARGLIST + eap->line1 - 1, ARGLIST + eap->line2,
-			(size_t)((ARGCOUNT - eap->line2) * sizeof(aentry_T)));
-	    ALIST(curwin)->al_ga.ga_len -= n;
-	    if (curwin->w_arg_idx >= eap->line2)
-		curwin->w_arg_idx -= n;
-	    else if (curwin->w_arg_idx > eap->line1)
-		curwin->w_arg_idx = eap->line1;
-	    if (ARGCOUNT == 0)
-		curwin->w_arg_idx = 0;
-	    else if (curwin->w_arg_idx >= ARGCOUNT)
-		curwin->w_arg_idx = ARGCOUNT - 1;
-	}
+
+	for (i = eap->line1; i <= eap->line2; ++i)
+	    vim_free(ARGLIST[i - 1].ae_fname);
+	mch_memmove(ARGLIST + eap->line1 - 1, ARGLIST + eap->line2,
+		    (size_t)((ARGCOUNT - eap->line2) * sizeof(aentry_T)));
+	ARGCOUNT -= n;
+	if (curwin->w_arg_idx >= eap->line2)
+	    curwin->w_arg_idx -= n;
+	else if (curwin->w_arg_idx > eap->line1)
+	    curwin->w_arg_idx = eap->line1;
     }
     else
 	do_arglist(eap->arg, AL_DEL, 0, FALSE);
+
+    if (ARGCOUNT == 0)
+	curwin->w_arg_idx = 0;
+    else if (curwin->w_arg_idx >= ARGCOUNT)
+	curwin->w_arg_idx = ARGCOUNT - 1;
+
     maketitle();
 }
 
