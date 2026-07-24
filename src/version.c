d44085b3e8eee3c848714fa8fc1949047ffe1757
Foxe Chen <chen.foxe@gmail.com>
2026-07-24 16:45:25 -0400

    fix version feature ordering

--- src/version.c
+++ src/version.c
@@ -462,6 +462,11 @@ static char *(features[]) =
 #else
 	"-packages",
 #endif
+#ifdef FEAT_PRINT_PANGO
+	"+pango",
+#else
+	"-pango",
+#endif
 	"+path_extra",
 #ifdef FEAT_PERL
 # ifdef DYNAMIC_PERL
@@ -487,11 +492,6 @@ static char *(features[]) =
 	"+postscript",
 # else
 	"-postscript",
-# endif
-# ifdef FEAT_PRINT_PANGO
-	"+pango",
-# else
-	"-pango",
 # endif
 	"+printer",
 #else
