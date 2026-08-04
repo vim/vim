f8b1774f4f77035eff0332d87770d156aec9fde4
Foxe Chen <chen.foxe@gmail.com>
2026-08-04 03:06:18 -0400

    make weston have fake seat

--- ci/setup-weston.sh
+++ ci/setup-weston.sh
@@ -8,7 +8,7 @@ cat <<EOT >/etc/systemd/system/weston.service
 Description=Weston Compositor Service
 After=network.target
 [Service]
-ExecStart=/usr/bin/weston --backend=headless --width=2560 --height=1440 --socket=/tmp/weston_sock
+ExecStart=/usr/bin/weston --backend=headless --fake-seat --width=5000 --height=5000 --socket=/tmp/weston_sock
 [Install]
 WantedBy=multi-user.target
 EOT
