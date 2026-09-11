df0e4a9951f4c7fb04b937369547ec1216545291
Christian Brabandt <cb@256bit.org>
2026-09-11 21:12:33 +0000

    Coverity: unchecked return value in mch_get_cmd_output_direct()
    Problem:  unchecked return value in mch_get_cmd_output_direct()
    Solution: break out of the loop, to avoid a heap buffer overflow
    
    closes: #PR
    related: #PR
    
    Signed-off-by: Christian Brabandt <cb@256bit.org>

--- src/os_unix.c
+++ src/os_unix.c
@@ -6027,7 +6027,8 @@ mch_get_cmd_output_direct(
 	n = (int)read(fd_out[0], buf, sizeof(buf));
 	if (n <= 0)
 	    break;
-	ga_grow(&ga, n);
+	if (ga_grow(&ga, n) == FAIL)
+	    break;
 	mch_memmove((char *)ga.ga_data + ga.ga_len, buf, n);
 	ga.ga_len += n;
     }
