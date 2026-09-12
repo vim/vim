aa8872bc2b81b07ac3dcb8e10b45a0323bed1a4e
Foxe Chen <chen.foxe@gmail.com>
2026-09-12 00:11:14 -0400

    use custom sixel encoder

--- src/image.h
+++ src/image.h
@@ -39,7 +39,10 @@ struct image_S
     int id;
     int refcount;
 
-    pixman_image_t  *image;
+    pixman_image_t  *image; // Remove later
+    uint8_t	    *data;
+    int		    width;
+    int		    height;
     image_format_T  fmt;
 
     image_backend_T backend;
